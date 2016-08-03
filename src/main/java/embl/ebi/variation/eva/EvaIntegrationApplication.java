21a2eeb592ff1093f3afff65fcbd193774fb58f3
Tom Smith <tsmith@ebi.ac.uk>
2016-08-03 15:41:21 +0100

    added files generated using spring boot

---
+++ src/main/java/embl/ebi/variation/eva/EvaIntegrationApplication.java
@@ -0,0 +1,12 @@
+package embl.ebi.variation.eva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaIntegrationApplication.class, args);
+	}
+}
---
+++ src/test/java/embl/ebi/variation/eva/EvaIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package embl.ebi.variation.eva;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EvaIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
