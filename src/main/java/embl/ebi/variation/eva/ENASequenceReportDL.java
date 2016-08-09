2e3cd9896ab06d5e43f6b6ddc19f7de2f82f49d1
Tom Smith <tsmith@ebi.ac.uk>
2016-08-09 09:32:50 +0100

    Moved main method to different class.

--- src/main/java/embl/ebi/variation/eva/ENASequenceReportDL.java
+++ src/main/java/embl/ebi/variation/eva/ENASequenceReportDL.java
@@ -1,21 +1,17 @@
 package embl.ebi.variation.eva;
 
+import static org.junit.Assert.assertTrue;
+
 import org.slf4j.LoggerFactory;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.StandardEnvironment;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.PollableChannel;
 import org.springframework.messaging.support.GenericMessage;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Properties;
 
 /**
@@ -26,27 +22,24 @@ public class ENASequenceReportDL {
     private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(ENASequenceReportDL.class);
 
 
-    public static void main(String[] args) {
+    public static void downloadSequenceReport(ConfigurableApplicationContext ctx, String assemblyAccession, String sequenceReportDirectory) {
 
-        Properties properties = loadProperties();
-
-        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext(
-                new String[] { "ena-inbound-config.xml" },
-                false);
-
-        setEnvironmentForAccession(ctx, "GCA_000001405.10");
+        setEnvironmentForAccession(ctx, assemblyAccession);
 
         ctx.refresh();
 
         MessageChannel inputChannel = ctx.getBean("inputChannel", MessageChannel.class);
-        inputChannel.send(new GenericMessage<String>(properties.getProperty("sequenceReportDirectory")));
+        PollableChannel outputChannel = ctx.getBean("outputChannel", PollableChannel.class);
+        inputChannel.send(new GenericMessage<String>(sequenceReportDirectory));
+
+//        assertTrue(outputChannel.receive().getPayload());
 
 
 //        PollableChannel filteredChannel = ctx.getBean("filteredChannel", PollableChannel.class);
 //        LOGGER.info("======>>>>> OUPUT FROM FILTER: " + filteredChannel.receive().getPayload());
 
 //        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext(
-//                "classpath:ena-inbound-config.xml");
+//                "classpath:eva-integration-config.xml");
 
 
 
@@ -68,16 +61,6 @@ public class ENASequenceReportDL {
         PropertiesPropertySource pps = new PropertiesPropertySource("ftpprops", props);
         env.getPropertySources().addFirst(pps);
         ctx.setEnvironment(env);
-    }
-
-    private static Properties loadProperties(){
-        Properties prop = new Properties();
-        try {
-            prop.load(ENASequenceReportDL.class.getClassLoader().getResourceAsStream("user.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return prop;
     }
 
 }
--- src/main/java/embl/ebi/variation/eva/EvaIntegrationApplication.java
+++ src/main/java/embl/ebi/variation/eva/EvaIntegrationApplication.java
@@ -2,11 +2,34 @@ package embl.ebi.variation.eva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
+import java.io.IOException;
+import java.util.Properties;
+
 public class EvaIntegrationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EvaIntegrationApplication.class, args);
+
+		ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext(
+				new String[] {"eva-integration-config.xml"},
+				false);
+
+		Properties properties = loadProperties();
+
+		ENASequenceReportDL.downloadSequenceReport(ctx, "GCA_000001405.10",
+				properties.getProperty("sequenceReportDirectory"));
+	}
+
+
+	private static Properties loadProperties(){
+		Properties prop = new Properties();
+		try {
+			prop.load(ENASequenceReportDL.class.getClassLoader().getResourceAsStream("user.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return prop;
 	}
 }
