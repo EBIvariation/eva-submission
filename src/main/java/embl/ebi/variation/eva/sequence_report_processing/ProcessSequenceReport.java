ba2d4d97e3a8d4af80db2406ce9161a24bc3aa6f
Tom Smith <tsmith@ebi.ac.uk>
2016-08-11 15:32:31 +0100

    - Test properties file and class - Test complete for process sequence report test

--- src/main/java/embl/ebi/variation/eva/sequence_report_processing/ProcessSequenceReport.java
+++ src/main/java/embl/ebi/variation/eva/sequence_report_processing/ProcessSequenceReport.java
@@ -2,7 +2,6 @@ package embl.ebi.variation.eva.sequence_report_processing;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
 import org.apache.log4j.Logger;
 
 import java.io.*;
@@ -17,10 +16,18 @@ public class ProcessSequenceReport {
 
     private Logger logger = Logger.getLogger(ProcessSequenceReport.class);
 
-    public List getChromosomeAccessions(File file){
+    public List<String> getChromosomeAccessions(File file){
         logger.info("Getting chromosome accessions from sequence report file: " + file);
 
-        CSVParser parser;
+        CSVParser parser = setUpCSVParser(file);
+
+        List<String> chromosomeAccessions = parseChromosomeAccessions(parser);
+
+        return chromosomeAccessions;
+    }
+
+    private CSVParser setUpCSVParser(File file){
+        CSVParser parser = null;
 
         try {
             parser = CSVParser.parse(file, StandardCharsets.UTF_8, CSVFormat.TDF);
@@ -30,16 +37,17 @@ public class ProcessSequenceReport {
             System.exit(1);
         }
 
-        List chromosomeAccessions = parseChromosomeAccessions(parser);
-
+        return parser;
     }
 
-    private List parseChromosomeAccessions(CSVParser parser){
-        List chromsomeAccessions = new ArrayList();
+    private List<String> parseChromosomeAccessions(CSVParser parser){
+        List<String> chromsomeAccessions = new ArrayList<String>();
 
-        for (CSVRecord csvRecord : parser){
+        parser.forEach(csvRecord -> chromsomeAccessions.add(csvRecord.get(0)));
 
-        }
+        chromsomeAccessions.remove(0); // remove the header element
+
+        return chromsomeAccessions;
     }
 
 
---
+++ src/test/java/embl/ebi/variation/eva/utils/TestProperties.java
@@ -0,0 +1,38 @@
+package embl.ebi.variation.eva.utils;
+
+import embl.ebi.variation.eva.sequence_report_download.ENASequenceReportDL;
+import embl.ebi.variation.eva.sequence_report_processing.ProcessSequenceReport;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by tom on 11/08/16.
+ */
+public class TestProperties {
+
+    private Logger logger = Logger.getLogger(ProcessSequenceReport.class);
+
+    public Properties properties;
+
+    private TestProperties(){
+
+        properties = new Properties();
+        try {
+            properties.load(ENASequenceReportDL.class.getClassLoader().getResourceAsStream("test.properties"));
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static class TestPropertiesHolder {
+        private static final TestProperties INSTANCE = new TestProperties();
+    }
+
+    public static TestProperties getInstance() {
+        return TestPropertiesHolder.INSTANCE;
+    }
+
+}
