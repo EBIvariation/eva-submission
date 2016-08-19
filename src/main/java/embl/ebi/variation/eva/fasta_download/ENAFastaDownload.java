e8f4f6e8569ac06a1e04336d80259d543f3f7dae
Tom Smith <tsmith@ebi.ac.uk>
2016-08-19 09:55:22 +0100

    Back to integration flows. Works fine and much less code, but also a lot clearer

--- src/main/java/embl/ebi/variation/eva/fasta_download/ENAFastaDownload.java
+++ src/main/java/embl/ebi/variation/eva/fasta_download/ENAFastaDownload.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.integration.annotation.*;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.channel.MessageChannels;
 import org.springframework.integration.dsl.core.Pollers;
 import org.springframework.integration.dsl.file.Files;
 import org.springframework.integration.dsl.http.Http;
@@ -16,6 +17,7 @@ import org.springframework.integration.file.FileWritingMessageHandler;
 import org.springframework.integration.file.support.FileExistsMode;
 import org.springframework.integration.http.outbound.HttpRequestExecutingMessageHandler;
 import org.springframework.integration.scheduling.PollerMetadata;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.io.File;
 import java.util.Collections;
@@ -26,67 +28,35 @@ import java.util.Collections;
 @Configuration
 public class ENAFastaDownload {
 
-//    @Autowired
-//    ThreadPoolTaskExecutor taskExecutor;
-//
-    @Bean(name = PollerMetadata.DEFAULT_POLLER)
-    public PollerMetadata poller(){
-        return Pollers
-                .fixedDelay(100)
-                .get();
-    }
-
     @Bean
-    @ServiceActivator(inputChannel = "channelToHttpRequest")
-    public HttpRequestExecutingMessageHandler httpMessageHandler(){
-        HttpRequestExecutingMessageHandler handler = new HttpRequestExecutingMessageHandler("https://www.ebi.ac.uk/ena/data/view/{chromAcc}&amp;display=fasta");
-        handler.setHttpMethod(HttpMethod.GET);
-        handler.setExpectedResponseType(java.lang.String.class);
-        SpelExpressionParser parser = new SpelExpressionParser();
-        handler.setUriVariableExpressions(Collections.singletonMap("chromAcc", parser.parseExpression("payload")));
-        handler.setOutputChannelName("channelToFileOutput");
-        return handler;
+    public ThreadPoolTaskExecutor taskExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(15);
+        return executor;
     }
-
-    @Bean
-    @ServiceActivator(inputChannel = "channelToFileOutput")
-    public FileWritingMessageHandler fileWritingMessageHandler(){
-        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File("/home/tom/Job_Working_Directory/Java/eva-integration/src/main/resources/test_dl/ftpInbound"));
-        handler.setFileExistsMode(FileExistsMode.APPEND);
-        handler.setFileNameGenerator(message -> "GCA_000001405.10.fasta2");
-        handler.setOutputChannelName("channelOutFastaDownload");
-        return handler;
-    }
-
-    @Bean
-    @Aggregator(inputChannel = "channelOutFastaDownload", outputChannel = "nullchannel")
-    public FileWritingOutputAggregator fileWritingOutputAggregator(){
-        return new FileWritingOutputAggregator();
-    }
-
-//    @Bean
-//    public ThreadPoolTaskExecutor taskExecutor(){
-//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-//        executor.setCorePoolSize(5);
-//        executor.setMaxPoolSize(15);
-//        return executor;
-//    }
 
     @Autowired
-    public SequenceReportProcessor sequenceReportProcessor;
+    private ThreadPoolTaskExecutor taskExecutor;
+
+    @Autowired
+    private SequenceReportProcessor sequenceReportProcessor;
 
     @Bean
     public IntegrationFlow fastaDownloadFlow() {
         return IntegrationFlows.from("channelIntoDownloadFastaENA")
                 .transform(sequenceReportProcessor, "getChromosomeAccessions")
                 .split()
+                .channel(MessageChannels.executor(taskExecutor))
                 .handle(Http.outboundGateway("https://www.ebi.ac.uk/ena/data/view/{payload}&amp;display=fasta")
                         .httpMethod(HttpMethod.GET)
                         .expectedResponseType(java.lang.String.class)
                         .uriVariable("payload", "payload"))
+                .channel(MessageChannels.queue(15))
                 .handle(Files.outboundGateway(new File("/home/tom/Job_Working_Directory/Java/eva-integration/src/main/resources/test_dl/ftpInbound"))
                         .fileExistsMode(FileExistsMode.APPEND)
-                        .fileNameGenerator(message -> "GCA_000001405.10.fasta2"))
+                        .fileNameGenerator(message -> "GCA_000001405.10.fasta2"),
+                    e -> e.poller(Pollers.fixedDelay(1000)))
                 .channel("nullChannel")
                 .get();
 
--- src/main/java/embl/ebi/variation/eva/sequence_report_processing/SequenceReportProcessor.java
+++ src/main/java/embl/ebi/variation/eva/sequence_report_processing/SequenceReportProcessor.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -13,6 +14,7 @@ import java.util.List;
 /**
  * Created by tom on 11/08/16.
  */
+@Component
 public class SequenceReportProcessor {
 
     private Logger logger = Logger.getLogger(SequenceReportProcessor.class);
