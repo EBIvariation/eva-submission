b91536da24068a98144dd0a29f616d87092ff11c
Tom Smith <tsmith@ebi.ac.uk>
2016-10-04 16:12:06 +0100

    Testing SequenceReportProcessor for valid and invalid chromosome accessions, in separate methods.

--- src/test/java/embl/ebi/variation/eva/fastadownload/SequenceReportProcessorTest.java
+++ src/test/java/embl/ebi/variation/eva/fastadownload/SequenceReportProcessorTest.java
@@ -3,11 +3,14 @@ package embl.ebi.variation.eva.fastadownload;
 import embl.ebi.variation.eva.utils.TestProperties;
 import org.apache.log4j.Logger;
 import org.hamcrest.collection.IsIterableContainingInAnyOrder;
+import org.junit.*;
 import org.junit.Assert;
-import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -20,21 +23,28 @@ public class SequenceReportProcessorTest {
 
     private static final String TEST_SEQUENCE_REPORT_FILENAME = "/GCA_000001405.23_sequence_report.txt";
 
-    @Test
-    public void getChromosomeAccessionsTest() throws Exception {
+    private List<String> chromosomeAccessions;
 
-        TestProperties testProperties = new TestProperties();
-        String[] testChromosomeAccessions = testProperties.getProperties().getProperty("seqreport.accessions").split(",");
-
+    @BeforeClass
+    public void setUp() throws IOException {
         SequenceReportProcessor sequenceReportProcessor = new SequenceReportProcessor();
         URL testSequenceReportFilePath = SequenceReportProcessorTest.class.getResource(TEST_SEQUENCE_REPORT_FILENAME);
         File testSequenceReportFile = new File(testSequenceReportFilePath.getFile());
-        List<String> chromosomeAccessions = sequenceReportProcessor.getChromosomeAccessions(testSequenceReportFile);
+        chromosomeAccessions = sequenceReportProcessor.getChromosomeAccessions(testSequenceReportFile);
+    }
 
-        Collections.sort(chromosomeAccessions);
+    @Test
+    public void getChromosomeAccessionsValidTest() throws Exception {
+        String[] validChromosomeAccessions = {"CM000663.2", "CM000664.2", "CM000665.2", "CM000666.2"};
+        List<String> validChromosomeAccessionsList = Arrays.asList(validChromosomeAccessions);
+        Assert.assertTrue(chromosomeAccessions.containsAll(validChromosomeAccessionsList));
+    }
 
-        Assert.assertThat(chromosomeAccessions, IsIterableContainingInAnyOrder.containsInAnyOrder(testChromosomeAccessions));
-
+    @Test
+    public void getChromosomeAccessionsInvalidTest() throws Exception {
+        String[] invalidChromosomeAccessions = {"JH159135.1", "GL582974.1", "CM000663.1", "CM000664.1"};
+        List<String> invalidChromosomeAccessionsList = Arrays.asList(invalidChromosomeAccessions);
+        Assert.assertFalse(chromosomeAccessions.containsAll(invalidChromosomeAccessionsList));
     }
 
 }
