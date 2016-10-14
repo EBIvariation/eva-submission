66f461f6526f9949c6c1480bf4513cbb8a1898da
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2016-10-14 14:01:29 +0100

    Fixed indentation of XXXProperties classes

--- src/main/java/embl/ebi/variation/eva/configuration/AssemblyDownloadProperties.java
+++ src/main/java/embl/ebi/variation/eva/configuration/AssemblyDownloadProperties.java
@@ -14,44 +14,46 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "assembly", ignoreUnknownFields = false)
 public class AssemblyDownloadProperties {
 
-	@NotNull @Pattern(regexp="GCA_\\d+(\\.\\d+)?")
-	private String accession;
+    @NotNull
+    @Pattern(regexp = "GCA_\\d+(\\.\\d+)?")
+    private String accession;
 
-	@NotNull @Size(min=1)
-	private String downloadRootPath;
-	
-	public AssemblyDownloadProperties() {}
-	
-	public AssemblyDownloadProperties(String accession, String downloadRootPath) {
-            this.accession = accession;
-            this.downloadRootPath = downloadRootPath;
-        }
-	
-        public String getAccession() {
-		return accession;
-	}
+    @NotNull
+    @Size(min = 1)
+    private String downloadRootPath;
 
-	public void setAccession(String accession) {
-		this.accession = accession;
-	}
+    public AssemblyDownloadProperties() { }
 
-	public String getDownloadRootPath() {
-		return downloadRootPath;
-	}
+    public AssemblyDownloadProperties(String accession, String downloadRootPath) {
+        this.accession = accession;
+        this.downloadRootPath = downloadRootPath;
+    }
 
-	public void setDownloadRootPath(String downloadRootPath) {
-		this.downloadRootPath = downloadRootPath;
-	}
+    public String getAccession() {
+        return accession;
+    }
 
-	public String getSequenceReportFileBasename() {
-		return accession + "_sequence_report.txt";
-	}
+    public void setAccession(String accession) {
+        this.accession = accession;
+    }
 
-	public Path getDownloadPath() {
-		return Paths.get(downloadRootPath, accession);
-	}
-	
-	public Path getFastaDownloadPath() {
-		return Paths.get(downloadRootPath, accession + ".fasta");
-	}
+    public String getDownloadRootPath() {
+        return downloadRootPath;
+    }
+
+    public void setDownloadRootPath(String downloadRootPath) {
+        this.downloadRootPath = downloadRootPath;
+    }
+
+    public String getSequenceReportFileBasename() {
+        return accession + "_sequence_report.txt";
+    }
+
+    public Path getDownloadPath() {
+        return Paths.get(downloadRootPath, accession);
+    }
+
+    public Path getFastaDownloadPath() {
+        return Paths.get(downloadRootPath, accession + ".fasta");
+    }
 }
--- src/main/java/embl/ebi/variation/eva/configuration/EnaFtpProperties.java
+++ src/main/java/embl/ebi/variation/eva/configuration/EnaFtpProperties.java
@@ -9,67 +9,71 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "ena-ftp", ignoreUnknownFields = false)
 public class EnaFtpProperties {
 
-	@NotNull @Size(min=1)
-	private String host;
+    @NotNull
+    @Size(min = 1)
+    private String host;
 
-	@Min(1) @Max(65535)
-	private int port;
-	
-	private String username;
-	
-	private String password;
+    @Min(1)
+    @Max(65535)
+    private int port;
 
-	@NotNull @Size(min=1)
-	private String sequenceReportRoot;
-	
-	public EnaFtpProperties() {}
-	
-        public EnaFtpProperties(String host, int port, String username, String password, String sequenceReportRoot) {
-            this.host = host;
-            this.port = port;
-            this.username = username;
-            this.password = password;
-            this.sequenceReportRoot = sequenceReportRoot;
-        }
+    private String username;
 
-        public String getHost() {
-		return host;
-	}
+    private String password;
 
-	public void setHost(String host) {
-		this.host = host;
-	}
+    @NotNull
+    @Size(min = 1)
+    private String sequenceReportRoot;
 
-	public int getPort() {
-		return port;
-	}
+    public EnaFtpProperties() {
+    }
 
-	public void setPort(int port) {
-		this.port = port;
-	}
+    public EnaFtpProperties(String host, int port, String username, String password, String sequenceReportRoot) {
+        this.host = host;
+        this.port = port;
+        this.username = username;
+        this.password = password;
+        this.sequenceReportRoot = sequenceReportRoot;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public String getHost() {
+        return host;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public void setHost(String host) {
+        this.host = host;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public int getPort() {
+        return port;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPort(int port) {
+        this.port = port;
+    }
 
-	public String getSequenceReportRoot() {
-		return sequenceReportRoot;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setSequenceReportRoot(String sequenceReportRoot) {
-		this.sequenceReportRoot = sequenceReportRoot;
-	}
-	
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSequenceReportRoot() {
+        return sequenceReportRoot;
+    }
+
+    public void setSequenceReportRoot(String sequenceReportRoot) {
+        this.sequenceReportRoot = sequenceReportRoot;
+    }
+
 }
