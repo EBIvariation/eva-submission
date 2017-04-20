36ec16168ab50edf014a8a3456fc4a250b4f052e
Gary Saunders <garys@ebi.ac.uk>
2017-04-20 11:48:44 +0100

    Updating code to address final comments from Tom

--- bin/eva_ftp_automation.py
+++ bin/eva_ftp_automation.py
@@ -9,20 +9,20 @@ parser = argparse.ArgumentParser(description='This script pulls a CSV list of fi
                                              'from the EVA FTP for a particular project, '
                                              'split by analysis accession and also gives the'
                                              'associated md5sum value for each file.')
-parser.add_argument('-p','--project_id', help='project_id to pull files from ERAPRO',
+parser.add_argument('-p', '--project_id', help='project_id to pull files from ERAPRO',
                     required=True, dest='project_id')
-parser.add_argument('-f','--outputdest', help='output file from running script', required=True,
+parser.add_argument('-f', '--outputdest', help='output file from running script', required=True,
                     dest='dest')
-parser.add_argument('-d','--db', help='database to connect to', required=True, dest='database')
-parser.add_argument('-u','--dbuname', help='username for database connection', required=True,
+parser.add_argument('-d', '--db', help='database to connect to', required=True, dest='database')
+parser.add_argument('-u', '--dbuname', help='username for database connection', required=True,
                     dest='user')
-parser.add_argument('-H','--hostname', help='host for database connection', required=True,
+parser.add_argument('-H', '--hostname', help='host for database connection', required=True,
                     dest='host')
-parser.add_argument('-P','--dbport', help='port for database connection', required=True,
+parser.add_argument('-P', '--dbport', help='port for database connection', required=True,
                     dest='port')
 args = parser.parse_args()
 
-my_pwd = getpass.getpass(stream=None)
+my_pwd = getpass.getpass()
 conn = psycopg2.connect(database=args.database, user=args.user, password=my_pwd, host=args.host,
                         port=args.port)
 cur = conn.cursor()
@@ -33,9 +33,10 @@ cur.execute("""SELECT project_accession, analysis_accession, filename, file_md5,
 
 records = cur.fetchall()
 
-with (open(args.dest, 'w')) as f:
-    writer = csv.writer (f, delimiter = ',')
+with open(args.dest, 'w') as f:
+    writer = csv.writer(f, delimiter=',')
     for row in records:
         writer.writerow(row)
 
+cur.close()
 conn.close()
