f2f2552ce2d79af7f4c555ec93d9e232a831a418
jose miguel mut <jomutlo@gmail.com>
2018-02-07 09:55:45 +0000

    improved vcf validation cron

--- bin/EVA-1018.sh
+++ bin/EVA-1018.sh
@@ -1,10 +1,28 @@
+#!/bin/bash
 
-for i in `find /nfs/ftp/private/eva-box-* -name "*vcf.gz" -mmin -1440`;
-do bsub "zcat $i | /nfs/production3/eva/software/vcf-validator/version-history/vcf_validator_0.7_summary -r summary,text,database -o /nfs/production3/eva/submissions/new_files_validations/"; done
-sleep 1m
-find /nfs/production3/eva/submissions/new_files_validations/ -name "*" -mmin -1400 > /nfs/production3/eva/submissions/new_files_validations/newfiles.txt 
-if [[ $(find /nfs/production3/eva/submissions/new_files_validations/newfiles.txt -mmin -1400) ]];
-then
-mutt -s "New VCFs for validation in FTP dropbox area" garys@ebi.ac.uk < /nfs/production3/eva/submissions/new_files_validations/newfiles.txt
-fi
+find /nfs/ftp/private/eva-box-* -name "*vcf.gz" -newer /nfs/production3/eva/submissions/new_files_validations/last_cron_execution.txt > /nfs/production3/eva/submissions/new_files_validations/uploading.txt
+
+while read line; do
+  if grep -q "$line" /nfs/production3/eva/submissions/new_files_validations/uploading.txt; then
+    # file in both files, this means it's still uploading
+    true # noop
+  else
+    # file was uploading, but not anymore: do validation
+    echo "$line" >> /nfs/production3/eva/submissions/new_files_validations/files_to_validate.txt
+  fi
+done < /nfs/production3/eva/submissions/new_files_validations/was_uploading_in_last_cron_execution.txt
+
+
+mutt -s "New VCFs for validation in FTP dropbox area" garys@ebi.ac.uk jmmut@ebi.ac.uk < /nfs/production3/eva/submissions/new_files_validations/files_to_validate.txt
+
+while read line; do
+  validation_folder="/nfs/production3/eva/submissions/new_files_validations/${line}.d/"
+  mkdir "$validation_folder"
+  bsub "zcat $line | /nfs/production3/eva/software/vcf-validator/version-history/vcf_validator_0.7_summary -r summary,text,database -o $validation_folder"
+done < /nfs/production3/eva/submissions/new_files_validations/files_to_validate.txt
+
+mv /nfs/production3/eva/submissions/new_files_validations/files_to_validate.txt /nfs/production3/eva/submissions/new_files_validations/files_validated_in_last_cron_execution.txt
+mv /nfs/production3/eva/submissions/new_files_validations/uploading.txt /nfs/production3/eva/submissions/new_files_validations/was_uploading_in_last_cron_execution.txt
+
+touch /nfs/production3/eva/submissions/new_files_validations/last_cron_execution.txt
 
