682ff9ceaf6bde8a4b08a31685a1e4f544d365d8
Jose Lopez <jmmut@ebi.ac.uk>
2018-02-13 11:10:40 +0000

    send always an email (even if no new files) and refactoring

--- bin/identify_new_vcfs_and_validate.sh
+++ bin/identify_new_vcfs_and_validate.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
 
-find /nfs/ftp/private/eva-box-* -name "*vcf.gz" -newer /nfs/production3/eva/submissions/new_files_validations/last_cron_execution.txt > /nfs/production3/eva/submissions/new_files_validations/uploading.txt
-find /nfs/ftp/private/eva-box-* -name "*vcf" -newer /nfs/production3/eva/submissions/new_files_validations/last_cron_execution.txt >> /nfs/production3/eva/submissions/new_files_validations/uploading.txt
+main_folder=/nfs/production3/eva/submissions/new_files_validations
 
-touch /nfs/production3/eva/submissions/new_files_validations/files_to_validate.txt
+find /nfs/ftp/private/eva-box-* -name "*vcf.gz" -newer ${main_folder}/last_cron_execution.txt > ${main_folder}/uploading.txt
+find /nfs/ftp/private/eva-box-* -name "*vcf" -newer ${main_folder}/last_cron_execution.txt >> ${main_folder}/uploading.txt
+
+touch ${main_folder}/files_to_validate.txt
 
 while read line; do
-  if grep -q "$line" /nfs/production3/eva/submissions/new_files_validations/uploading.txt; then
+  if grep -q "$line" ${main_folder}/uploading.txt; then
     # file in both files, this means it's still uploading
     true # noop
   else
     # file was uploading, but not anymore: do validation
-    echo "$line" >> /nfs/production3/eva/submissions/new_files_validations/files_to_validate.txt
+    echo "$line" >> ${main_folder}/files_to_validate.txt
   fi
-done < /nfs/production3/eva/submissions/new_files_validations/was_uploading_in_last_cron_execution.txt
+done < ${main_folder}/was_uploading_in_last_cron_execution.txt
 
 
-if [ -s /nfs/production3/eva/submissions/new_files_validations/files_to_validate.txt ]; then 
-  mutt -s "New VCFs for validation in FTP dropbox area" garys@ebi.ac.uk jmmut@ebi.ac.uk < /nfs/production3/eva/submissions/new_files_validations/files_to_validate.txt
-else 
-  # file was empty, so don't send an email
-  true # noop
-fi
+mutt -s "New VCFs for validation in FTP dropbox area" eva-dev@ebi.ac.uk < ${main_folder}/files_to_validate.txt
 
 
 while read line; do
-  validation_folder="/nfs/production3/eva/submissions/new_files_validations/${line}.d/"
+  validation_folder="${main_folder}/${line}.d/"
   mkdir -p "$validation_folder"
   bsub "zcat -f $line | /nfs/production3/eva/software/vcf-validator/version-history/vcf_validator_0.7_summary -r summary,text,database -o $validation_folder"
-done < /nfs/production3/eva/submissions/new_files_validations/files_to_validate.txt
+done < ${main_folder}/files_to_validate.txt
 
-mv /nfs/production3/eva/submissions/new_files_validations/files_to_validate.txt /nfs/production3/eva/submissions/new_files_validations/files_validated_in_last_cron_execution.txt
-mv /nfs/production3/eva/submissions/new_files_validations/uploading.txt /nfs/production3/eva/submissions/new_files_validations/was_uploading_in_last_cron_execution.txt
+mv ${main_folder}/files_to_validate.txt ${main_folder}/files_validated_in_last_cron_execution.txt
+mv ${main_folder}/uploading.txt ${main_folder}/was_uploading_in_last_cron_execution.txt
 
-touch /nfs/production3/eva/submissions/new_files_validations/last_cron_execution.txt
+touch ${main_folder}/last_cron_execution.txt
 
