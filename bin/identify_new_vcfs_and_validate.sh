51942a5b1f2b5f85a730b9bc0a5992964a9b5723
jose miguel mut <jomutlo@gmail.com>
2018-02-13 11:50:32 +0000

    more documentation and renaming

--- bin/identify_new_vcfs_and_validate.sh
+++ bin/identify_new_vcfs_and_validate.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 
-main_folder=/nfs/production3/eva/submissions/new_files_validations
+# This script checks whether there are new uploaded VCFs and runs vcf-validator over them.
+#
+# uploading.txt: files that were modified in the last period between the previous execution of this script and this execution.
+# files_to_validate.txt: files that were uploading, but were not modified in the last period. these will be validated.
+# uploading_in_last_cron_execution.txt: files that were modified in the previous period (not the last period)
+# files_validated_in_last_cron_execution.txt: for human checks, the validation reports should be available for these VCFs.
+# last_cron_execution.txt: empty file that is used as timestamp of the last execution of this script.
 
-find /nfs/ftp/private/eva-box-* -name "*vcf.gz" -newer ${main_folder}/last_cron_execution.txt > ${main_folder}/uploading.txt
-find /nfs/ftp/private/eva-box-* -name "*vcf" -newer ${main_folder}/last_cron_execution.txt >> ${main_folder}/uploading.txt
+root_folder=/nfs/production3/eva/submissions/new_files_validations
 
-touch ${main_folder}/files_to_validate.txt
+find /nfs/ftp/private/eva-box-* -name "*vcf.gz" -o -name "*vcf" -newer ${root_folder}/last_cron_execution.txt > ${root_folder}/uploading.txt
+
+touch ${root_folder}/files_to_validate.txt
 
 while read line; do
-  if grep -q "$line" ${main_folder}/uploading.txt; then
-    # file in both files, this means it's still uploading
+  if grep -q "$line" ${root_folder}/uploading.txt; then
+    # VCF pending validation but still uploading
     true # noop
   else
-    # file was uploading, but not anymore: do validation
-    echo "$line" >> ${main_folder}/files_to_validate.txt
+    # VCF was uploading, but not anymore: do validation
+    echo "$line" >> ${root_folder}/files_to_validate.txt
   fi
-done < ${main_folder}/was_uploading_in_last_cron_execution.txt
+done < ${root_folder}/uploading_in_last_cron_execution.txt
 
 
-mutt -s "New VCFs for validation in FTP dropbox area" eva-dev@ebi.ac.uk < ${main_folder}/files_to_validate.txt
+mutt -s "New VCFs ready for validation in FTP dropbox area" eva-dev@ebi.ac.uk < ${root_folder}/files_to_validate.txt
 
 
 while read line; do
-  validation_folder="${main_folder}/${line}.d/"
+  validation_folder="${root_folder}/${line}.d/"
   mkdir -p "$validation_folder"
   bsub "zcat -f $line | /nfs/production3/eva/software/vcf-validator/version-history/vcf_validator_0.7_summary -r summary,text,database -o $validation_folder"
-done < ${main_folder}/files_to_validate.txt
+done < ${root_folder}/files_to_validate.txt
 
-mv ${main_folder}/files_to_validate.txt ${main_folder}/files_validated_in_last_cron_execution.txt
-mv ${main_folder}/uploading.txt ${main_folder}/was_uploading_in_last_cron_execution.txt
+mv ${root_folder}/files_to_validate.txt ${root_folder}/files_validated_in_last_cron_execution.txt
+mv ${root_folder}/uploading.txt ${root_folder}/uploading_in_last_cron_execution.txt
 
-touch ${main_folder}/last_cron_execution.txt
+touch ${root_folder}/last_cron_execution.txt
 
