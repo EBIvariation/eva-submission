ccdf55f2751419b36564a7978e2ff50b77e129c5
sundarvenkata <sundarvenkata@ebi.ac.uk>
2018-08-07 15:27:10 +0100

    Removed check_samples_eva.sh per Cristina's suggestion

--- bin/samples_checker/check_samples_eva.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-NUM_ARGS=$#;
-if [ ${NUM_ARGS} -ne 4 ]; then
-    echo "Usage: check_samples_eva.sh <Path to samples_checker module> <Path to xls2xml module> <Path to metadata sheet> <Path to directory with the VCF files>";
-    exit 1;
-fi
-
-CURR_SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )";
-cd ${CURR_SCRIPT_DIR}/../.. && git pull origin master && cd $1 && git pull origin master && cd $2 && git pull origin master && cd ${CURR_SCRIPT_DIR} && source venv/bin/activate && python check_samples_eva.py --samples-checker-dir $1 --xls2xml-dir $2 --metadata-file $3 --vcf-files-path $4;
