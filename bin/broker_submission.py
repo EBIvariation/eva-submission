031c5e2b5e4d15f06796a1e15a99b6d9e04be4a4
Timothee Cezard <tcezard@ebi.ac.uk>
2021-01-18 21:36:12 +0000

    Do not repeat execution of tasks unless forced by command line

--- bin/broker_submission.py
+++ bin/broker_submission.py
@@ -35,7 +35,11 @@ def main():
                           help='Set the script to output logging information at debug level')
     argparse.add_argument('--vcf_files', required=False, type=str, help='VCF files to use in the brokering', nargs='+')
     argparse.add_argument('--metadata_file', required=False, type=str, help='VCF files to use in the brokering')
-
+    argparse.add_argument('--force', required=False, type=str, nargs='*', const=EloadBrokering.all_brokering_tasks,
+                          choices=EloadBrokering.all_brokering_tasks, default=[],
+                          help='When not set, the script only perfomred the tasks that were not successful. When '
+                               'set without argument force all the task to be performed regardless of previous status. '
+                               'Otherwise, you can specify one or several tasks to perform during the brokering')
     args = argparse.parse_args()
 
     log_cfg.add_stdout_handler()
@@ -47,7 +51,7 @@ def main():
 
     # Optionally Set the valid VCF and metadata file
     brokering = EloadBrokering(args.eload, args.vcf_files, args.metadata_file)
-    brokering.broker()
+    brokering.broker(args.force)
 
 
 if __name__ == "__main__":
