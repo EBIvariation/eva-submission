51316514b1a96221f375feb672cf525237bfd5fd
Timothee Cezard <tcezard@ebi.ac.uk>
2021-04-01 11:41:40 +0100

    Address review comments

--- tests/test_eload_submission.py
+++ tests/test_eload_submission.py
@@ -31,43 +31,38 @@ class TestEload(TestCase):
             shutil.rmtree(eload)
 
     def test_copy_from_ftp(self):
-        eload = EloadPreparation(1)
-        assert os.listdir(os.path.join(eload.eload_dir, '10_submitted', 'vcf_files')) == []
-        assert os.listdir(os.path.join(eload.eload_dir, '10_submitted', 'metadata_file')) == []
-        eload.copy_from_ftp(1, 'john')
-        assert os.listdir(os.path.join(eload.eload_dir, '10_submitted', 'vcf_files')) == ['data.vcf.gz']
-        assert os.listdir(os.path.join(eload.eload_dir, '10_submitted', 'metadata_file')) == ['metadata.xlsx']
+        assert os.listdir(os.path.join(self.eload.eload_dir, '10_submitted', 'vcf_files')) == []
+        assert os.listdir(os.path.join(self.eload.eload_dir, '10_submitted', 'metadata_file')) == []
+        self.eload.copy_from_ftp(1, 'john')
+        assert os.listdir(os.path.join(self.eload.eload_dir, '10_submitted', 'vcf_files')) == ['data.vcf.gz']
+        assert os.listdir(os.path.join(self.eload.eload_dir, '10_submitted', 'metadata_file')) == ['metadata.xlsx']
 
     def test_detect_submitted_metadata(self):
-        # create the eload
-        eload = EloadPreparation(1)
         # create the data
-        vcf1 = os.path.join(eload.eload_dir, '10_submitted', 'vcf_files', 'file1.vcf')
-        vcf2 = os.path.join(eload.eload_dir, '10_submitted', 'vcf_files', 'file2.vcf')
+        vcf1 = os.path.join(self.eload.eload_dir, '10_submitted', 'vcf_files', 'file1.vcf')
+        vcf2 = os.path.join(self.eload.eload_dir, '10_submitted', 'vcf_files', 'file2.vcf')
         touch(vcf1)
         touch(vcf2)
-        metadata = os.path.join(eload.eload_dir, '10_submitted', 'metadata_file', 'metadata.xlsx')
+        metadata = os.path.join(self.eload.eload_dir, '10_submitted', 'metadata_file', 'metadata.xlsx')
         touch(metadata)
 
-        eload.detect_submitted_vcf()
+        self.eload.detect_submitted_vcf()
         # Check that the vcf are in the config file
-        assert sorted(eload.eload_cfg.query('submission', 'vcf_files')) == [vcf1, vcf2]
+        assert sorted(self.eload.eload_cfg.query('submission', 'vcf_files')) == [vcf1, vcf2]
 
-        eload.detect_submitted_metadata()
+        self.eload.detect_submitted_metadata()
         # Check that the metadata spreadsheet is in the config file
-        assert eload.eload_cfg.query('submission', 'metadata_spreadsheet') == metadata
+        assert self.eload.eload_cfg.query('submission', 'metadata_spreadsheet') == metadata
 
     def test_replace_values_in_metadata(self):
-        # create the eload
-        eload = EloadPreparation(1)
         source_metadata = os.path.join(self.resources_folder, 'metadata.xlsx')
-        metadata = os.path.join(eload.eload_dir, '10_submitted', 'metadata_file', 'metadata.xlsx')
+        metadata = os.path.join(self.eload.eload_dir, '10_submitted', 'metadata_file', 'metadata.xlsx')
         shutil.copyfile(source_metadata, metadata)
 
         reader = EvaXlsxReader(metadata)
         assert reader.project['Tax ID'] == 9606
         assert reader.analysis[0]['Reference'] == 'GCA_000001405.1'
-        eload.replace_values_in_metadata(taxid=10000, reference_accession='GCA_000009999.9')
+        self.eload.replace_values_in_metadata(taxid=10000, reference_accession='GCA_000009999.9')
         reader = EvaXlsxReader(metadata)
         assert reader.project['Tax ID'] == 10000
         assert reader.analysis[0]['Reference'] == 'GCA_000009999.9'
