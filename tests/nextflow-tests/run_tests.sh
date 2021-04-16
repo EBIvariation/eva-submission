41fb009d9d80e1e16fb94c0d22efe54d7036c1b6
April Shen <ashen@ebi.ac.uk>
2021-04-16 16:36:14 +0100

    fix nextflow tests

--- tests/nextflow-tests/run_tests.sh
+++ tests/nextflow-tests/run_tests.sh
@@ -7,7 +7,7 @@ SOURCE_DIR="$(dirname $(dirname $SCRIPT_DIR))/nextflow"
 
 cwd=${PWD}
 cd ${SCRIPT_DIR}
-mkdir project project/logs project/accessions project/public ftp
+mkdir project project/accessions project/public ftp
 
 # run accession and variant load
 # note public_dir needs to be an absolute path, unlike others in config
@@ -24,7 +24,6 @@ printf "\e[32m====== Files made public ======\e[0m\n"
 ls project/public
 printf "\n\e[32m======== Commands run ========\e[0m\n"
 find work/ \( -name '*.out' -o -name '*.err' \) -exec cat {} \;
-cat project/logs/*.log
 
 # clean up
 rm -rf work .nextflow*
