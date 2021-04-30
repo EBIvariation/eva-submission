664edef58d2b4026b3ac04dbaa08b5bccbf441a5
April Shen <ashen@ebi.ac.uk>
2021-04-30 09:43:09 +0100

    use primary mongo host

--- eva_submission/eload_utils.py
+++ eva_submission/eload_utils.py
@@ -68,7 +68,9 @@ def get_metadata_conn():
 
 def get_mongo_creds():
     properties = get_properties_from_xml_file(cfg['maven']['environment'], cfg['maven']['settings_file'])
-    mongo_host = split_hosts(properties['eva.mongo.host'])[0][0]
+    # Use the primary mongo host from configuration:
+    # https://github.com/EBIvariation/configuration/blob/master/eva-maven-settings.xml#L111
+    mongo_host = split_hosts(properties['eva.mongo.host'])[1][0]
     mongo_user = properties['eva.mongo.user']
     mongo_pass = properties['eva.mongo.passwd']
     return mongo_host, mongo_user, mongo_pass
