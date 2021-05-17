5bc5fea9e41058626d6ad46514d4731133ff09cb
April Shen <april.tuesday@gmail.com>
2021-05-17 09:49:59 +0100

    Update prepare_backlog_study.py

--- bin/prepare_backlog_study.py
+++ bin/prepare_backlog_study.py
@@ -21,9 +21,9 @@ from argparse import ArgumentParser
 
 from ebi_eva_common_pyutils.logger import logging_config as log_cfg
 
-from eva_submission.eload_backlog import EloadBacklog
-
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from eva_submission.eload_backlog import EloadBacklog
 from eva_submission.eload_validation import EloadValidation
 from eva_submission.submission_config import load_config
 
