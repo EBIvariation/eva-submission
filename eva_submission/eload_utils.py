e861fb1b8585178c38823f71ad2c18c0751e4c1e
nkumar2 <nkumar2@ebi.ac.uk>
2021-07-07 09:47:54 +0100

    getting vep version from emsembl

--- eva_submission/eload_utils.py
+++ eva_submission/eload_utils.py
@@ -217,7 +217,7 @@ def get_vep_and_vep_cache_version_from_db(mongo_uri, db_name):
             f'Found following vep_version and vep_cache_versions from DB {vep_version_list}. Latest version is {latest_version}')
         return latest_version
 
-#TODO : add logic to get vep_version also
+
 def get_vep_and_vep_cache_version_from_ensembl(assembly_accession):
     try:
         logger.info('Getting vep and vep_cache_version from ensembl.')
@@ -231,23 +231,23 @@ def get_vep_and_vep_cache_version_from_ensembl(assembly_accession):
         logger.info(f'fetched all releases: {all_releases}')
 
         for release in sorted(all_releases, reverse=True):
-            logger.info(f'looking for vep_cache_version in release : {all_releases.get(release)}')
+            logger.info(f'looking for vep and vep_cache_version in release : {all_releases.get(release)}')
             all_species_files = get_all_species_files(ftp, all_releases.get(release))
             for file in all_species_files:
                 if species_assembly['species'] in file and species_assembly['assembly'] in file:
                     logger.info(
-                        f'Found vep_cache_version for the species and assembly : {species_assembly}, file: {file}, release: {release}')
+                        f'Found vep and vep_cache_version for the species and assembly : {species_assembly}, file: {file}, release: {release}')
                     return {
-                        "vep_version": None,
+                        "vep_version": release,
                         "vep_cache_version": release
                     }
-        logger.warn(f'could not find vep_cache_version for the given species and assembly: {species_assembly}')
+        logger.warning(f'could not find vep and vep_cache_version for the given species and assembly: {species_assembly}')
         return {
             "vep_version": None,
             "vep_cache_version": None
         }
     except Exception as err:
-        logger.warn(f'Encountered Error while fetching vep_cache_version : {err}')
+        logger.warning(f'Encountered Error while fetching vep and vep_cache_version : {err}')
         return {
             "vep_version": None,
             "vep_cache_version": None
@@ -258,7 +258,6 @@ def get_vep_and_vep_cache_version_from_ensembl(assembly_accession):
 def get_species_name_and_assembly(assembly_accession):
     url = f'https://rest.ensembl.org/info/genomes/assembly/{assembly_accession}?content-type=application/json'
     response = requests.get(url)
-    response.raise_for_status()
     json_response = response.json()
     if "error" in json_response:
         raise Exception(json_response["error"])
