4ccaec9ccf3d216d3c8d60de39246a068bb77102
Timothee Cezard <tcezard@ebi.ac.uk>
2021-07-19 13:15:46 +0200

    Addresses comments in review attempt to run the netflow test in Github actions

--- tests/test_eload_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-import unittest
-
-from eva_submission import ROOT_DIR
-
-
-class TestEloadUtils(unittest.TestCase):
-    resources_folder = os.path.join(ROOT_DIR, 'tests', 'resources')
-    pass
