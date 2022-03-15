7a7a5be8c6095805fe4e3fc4439d86837964956f
Timothee Cezard <tcezard@ebi.ac.uk>
2022-03-15 18:30:09 +0000

    Add tests and fixes some issues

--- eva_submission/ena_retrieval.py
+++ eva_submission/ena_retrieval.py
@@ -14,14 +14,14 @@ logger = log_cfg.get_logger(__name__)
 def files_from_ena(search_term):
     xml_root = download_xml_from_ena(f'https://www.ebi.ac.uk/ena/browser/api/xml/textsearch?result=analysis&query={search_term}')
     analyses = xml_root.xpath('/ANALYSIS_SET/ANALYSIS')
-    analysis_files = []
+    analysis_files = {}
     for analysis in analyses:
 
         files = analysis.xpath('FILES/FILE')
         file_dicts = []
         for file in files:
             file_dicts.append({'filename': file.attrib['filename'], 'filetype': file.attrib['filetype'],
-                               'md5': file.attrib['checksum']})
+                               'md5': file.attrib['checksum'], 'analysis_accession': analysis.attrib['accession']})
         analysis_files[analysis.attrib['accession']] = file_dicts
     return analysis_files
 
@@ -32,11 +32,10 @@ def get_file_from_md5(md5):
         rows = get_all_results_for_query(conn, query)
         file_ids = [file_id for file_id, in rows]
         if len(file_ids) > 1:
-            raise Exception(f'Multiple file found with md5 {md5}')
+            raise ValueError(f'Multiple file found with md5 {md5}')
         elif len(file_ids) == 0:
             return None
         return file_ids[0]
-        return file_ids
 
 
 def create_file(file_dict):
@@ -52,7 +51,7 @@ def create_file(file_dict):
 
     query = ('insert into file '
              '(filename, file_md5, file_type,  file_class, file_version, is_current, file_location, ftp_file) '
-             f"values ('{filename}', '{file_dict['md5']}', {file_type}, 'submitted', 1, 1, "
+             f"values ('{filename}', '{file_dict['md5']}', '{file_type}', 'submitted', 1, 1, "
              f"'{file_location}', '{ftp_file}')")
     with get_metadata_connection_handle(cfg['maven']['environment'], cfg['maven']['settings_file']) as conn:
         logger.info(f'Create file {filename} in the file table')
@@ -61,7 +60,7 @@ def create_file(file_dict):
 
 def create_file_in_analysis(file_dict):
     query = (f"insert into analysis_file (ANALYSIS_ACCESSION,FILE_ID) "
-             f"values ('{file_dict['file_id']}', '{file_dict['analysis_accession']}')")
+             f"values ({file_dict['file_id']}, '{file_dict['analysis_accession']}')")
     with get_metadata_connection_handle(cfg['maven']['environment'], cfg['maven']['settings_file']) as conn:
         logger.info(f"Create file {file_dict['file_id']} in the analysis_file table for '{file_dict['analysis_accession']}'")
         execute_query(conn, query)
@@ -69,7 +68,7 @@ def create_file_in_analysis(file_dict):
 
 def remove_file_from_analysis(file_dict):
     query = (f"delete from analysis_file "
-             f"where file_id='{file_dict['file_id']}' and analysis_accession='{file_dict['analysis_accession']}'")
+             f"where file_id={file_dict['file_id']} and analysis_accession='{file_dict['analysis_accession']}'")
     with get_metadata_connection_handle(cfg['maven']['environment'], cfg['maven']['settings_file']) as conn:
         logger.info(f"Remove file {file_dict['file_id']} from the analysis_file table for '{file_dict['analysis_accession']}'")
         execute_query(conn, query)
@@ -78,22 +77,23 @@ def remove_file_from_analysis(file_dict):
 def difference_evapro_file_set_with_ena_for_analysis(analysis_accession, ena_list_of_file_dicts):
     query = f"select f.file_name, f.file_md5 " \
             f"from analysis_file af join file f on af.file_id=f.file_id " \
-            f"where analysis_accession='{analysis_accession}';"
+            f"where af.analysis_accession='{analysis_accession}';"
     with get_metadata_connection_handle(cfg['maven']['environment'], cfg['maven']['settings_file']) as conn:
         eva_list_of_file_dicts = [dict(filename=fn, md5=md5) for fn, md5 in get_all_results_for_query(conn, query)]
-        set_of_file_from_ena = [d.get('md5') for d in ena_list_of_file_dicts]
-        set_of_file_from_eva_pro = [d.get('md5') for d in eva_list_of_file_dicts]
+        set_of_file_from_ena = set([d.get('md5') for d in ena_list_of_file_dicts])
+        set_of_file_from_eva_pro = set([d.get('md5') for d in eva_list_of_file_dicts])
         if set_of_file_from_ena != set_of_file_from_eva_pro:
             logger.warn(f'File for analysis {analysis_accession} are different in ENA and EVA')
             file_specific_to_ena = set_of_file_from_ena.difference(set_of_file_from_eva_pro)
-            for filename, md5 in file_specific_to_ena:
-                logger.warn(f'File {filename} exist in ENA but not in EVA.')
             file_specific_to_eva = set_of_file_from_eva_pro.difference(set_of_file_from_ena)
-            for filename, md5 in file_specific_to_eva:
-                logger.warn(f'File {filename} exist in EVA but not in ENA.')
-            return [file_dict for file_dict in set_of_file_from_ena if file_dict['md5'] in file_specific_to_ena],\
-                   [file_dict for file_dict in set_of_file_from_eva_pro if file_dict['md5'] in file_specific_to_eva]
-    return None, None
+            file_dict_specific_to_ena = [file_dict for file_dict in ena_list_of_file_dicts if file_dict['md5'] in file_specific_to_ena]
+            file_dict_specific_to_eva = [file_dict for file_dict in eva_list_of_file_dicts if file_dict['md5'] in file_specific_to_eva]
+            for file_dict in file_dict_specific_to_ena:
+                logger.warn(f"File {file_dict['filename']} exist in ENA but not in EVA.")
+            for file_dict in file_dict_specific_to_eva:
+                logger.warn(f"File {file_dict['filename']} exist in EVA but not in ENA.")
+            return file_dict_specific_to_ena, file_dict_specific_to_eva
+    return [], []
 
 
 def retrieve_files_from_ena(analysis_accession):
--- tests/test_ena_retrieval.py
+++ tests/test_ena_retrieval.py
@@ -1,11 +1,100 @@
+import os
 from unittest import TestCase
+from unittest.mock import patch
 
-from eva_submission.ena_retrieval import files_from_ena_project
+from eva_submission.ena_retrieval import files_from_ena, get_file_from_md5, create_file, create_file_in_analysis, \
+    remove_file_from_analysis, difference_evapro_file_set_with_ena_for_analysis
+from eva_submission.submission_config import load_config
 
 
 class TestEnaRetreival(TestCase):
+    resources_folder = os.path.join(os.path.dirname(__file__), 'resources')
 
-    def test_files_from_ena_project(self):
-        # files_from_ena_project('PRJEB21794')
-        files_from_ena_project('ERZ468492')
-        # files_from_ena_project('PRJEB28874')
+    def setUp(self):
+        config_file = os.path.join(self.resources_folder, 'submission_config.yml')
+        load_config(config_file)
+
+    def test_files_from_ena(self):
+        expected_files = {
+            'ERZ468492': [
+                {'filename': 'ERZ468/ERZ468492/OMIA001271_9913_2.vcf.gz.tbi', 'analysis_accession': 'ERZ468492',
+                 'filetype': 'tabix', 'md5': '07f98ac44d6d6f1453d40dc61f29ecec'},
+                {'filename': 'ERZ468/ERZ468492/OMIA001271_9913_2.vcf.gz',  'analysis_accession': 'ERZ468492',
+                 'filetype': 'vcf', 'md5': '5118c4d13159750f476005d64fc27829'}
+            ]
+        }
+        assert files_from_ena('ERZ468492') == expected_files
+
+    def test_get_file_from_md5(self):
+        with patch('eva_submission.ena_retrieval.get_metadata_connection_handle', autospec=True), \
+             patch('eva_submission.ena_retrieval.get_all_results_for_query') as m_get_results:
+            m_get_results.return_value = [('file_id',)]
+            assert get_file_from_md5('07f98ac44d6d6f1453d40dc61f29ecec') == 'file_id'
+            assert m_get_results.mock_calls[0][1][1] == \
+                   "select file_id from file where file_md5='07f98ac44d6d6f1453d40dc61f29ecec'"
+
+    def test_get_file_from_md5_duplicates(self):
+        with patch('eva_submission.ena_retrieval.get_metadata_connection_handle', autospec=True), \
+             patch('eva_submission.ena_retrieval.get_all_results_for_query') as m_get_results:
+            m_get_results.return_value = [('file_id1',), ('file_id2',)]
+            with self.assertRaises(ValueError):
+                get_file_from_md5('07f98ac44d6d6f1453d40dc61f29ecec')
+
+    def test_create_file(self):
+        with patch('eva_submission.ena_retrieval.get_metadata_connection_handle', autospec=True), \
+                patch('eva_submission.ena_retrieval.execute_query') as m_execute_query:
+            create_file({'filename': 'analysis_id/test.vcf.gz', 'md5': '07f98ac44d6d6f1453d40dc61f29ecec'})
+            expected_query = ("insert into file (filename, file_md5, file_type,  file_class, file_version, is_current, file_location, ftp_file) "
+                              "values ('test.vcf.gz', '07f98ac44d6d6f1453d40dc61f29ecec', 'vcf', 'submitted', 1, 1, 'scratch_folder', 'ftp.sra.ebi.ac.uk/vol1/analysis_id/test.vcf.gz')")
+            assert m_execute_query.mock_calls[0][1][1] == expected_query
+
+    def test_create_file_in_analysis(self):
+        with patch('eva_submission.ena_retrieval.get_metadata_connection_handle', autospec=True), \
+                patch('eva_submission.ena_retrieval.execute_query') as m_execute_query:
+            create_file_in_analysis({'file_id': 1234, 'analysis_accession': 'analysis1'})
+            expected_query = "insert into analysis_file (ANALYSIS_ACCESSION,FILE_ID) values (1234, 'analysis1')"
+            assert m_execute_query.mock_calls[0][1][1] == expected_query
+
+    def test_remove_file_from_analysis(self):
+        with patch('eva_submission.ena_retrieval.get_metadata_connection_handle', autospec=True), \
+                patch('eva_submission.ena_retrieval.execute_query') as m_execute_query:
+            remove_file_from_analysis({'file_id': 1234, 'analysis_accession': 'analysis1'})
+            expected_query = "delete from analysis_file where file_id=1234 and analysis_accession='analysis1'"
+            assert m_execute_query.mock_calls[0][1][1] == expected_query
+
+    def test_difference_evapro_file_set_with_ena_for_analysis_no_diff(self):
+        analysis_accession = 'analysis1'
+        ena_list_of_file_dicts = [
+            {'filename': 'analysis_id/test.vcf.gz', 'md5': '07f98ac44d6d6f1453d40dc61f29ecec'}
+        ]
+        with patch('eva_submission.ena_retrieval.get_metadata_connection_handle', autospec=True), \
+                patch('eva_submission.ena_retrieval.get_all_results_for_query') as m_get_results:
+            m_get_results.return_value = [('test.vcf.gz', '07f98ac44d6d6f1453d40dc61f29ecec')]
+            assert difference_evapro_file_set_with_ena_for_analysis(analysis_accession, ena_list_of_file_dicts) \
+                == ([], [])
+
+    def test_difference_plus_ena(self):
+        analysis_accession = 'analysis1'
+        ena_list_of_file_dicts = [
+            {'filename': 'analysis_id/test.vcf.gz', 'md5': '07f98ac44d6d6f1453d40dc61f29ecec'}
+        ]
+        with patch('eva_submission.ena_retrieval.get_metadata_connection_handle', autospec=True), \
+                patch('eva_submission.ena_retrieval.get_all_results_for_query') as m_get_results:
+            m_get_results.return_value = []
+            assert difference_evapro_file_set_with_ena_for_analysis(analysis_accession, ena_list_of_file_dicts) \
+                   == (ena_list_of_file_dicts, [])
+
+    def test_difference_plus_eva(self):
+        analysis_accession = 'analysis1'
+        ena_list_of_file_dicts = [
+            {'filename': 'analysis_id/test.vcf.gz', 'md5': '07f98ac44d6d6f1453d40dc61f29ecec'}
+        ]
+        with patch('eva_submission.ena_retrieval.get_metadata_connection_handle', autospec=True), \
+                patch('eva_submission.ena_retrieval.get_all_results_for_query') as m_get_results:
+            m_get_results.return_value = [
+                ('test.vcf.gz', '07f98ac44d6d6f1453d40dc61f29ecec'),
+                ('test2.vcf.gz', '8dhks0fhhwu83h99sk94762hs889sj83')
+            ]
+            assert difference_evapro_file_set_with_ena_for_analysis(analysis_accession, ena_list_of_file_dicts) \
+                   == ([], [{'filename': 'test2.vcf.gz', 'md5': '8dhks0fhhwu83h99sk94762hs889sj83'}])
+
