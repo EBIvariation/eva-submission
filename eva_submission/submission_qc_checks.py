15033a5c1198f157c091fcf66fc23e1466102d0a
tcezard <tcezard@ebi.ac.uk>
2024-02-05 16:27:37 +0000

    Default to 'none' when not set

--- eva_submission/submission_qc_checks.py
+++ eva_submission/submission_qc_checks.py
@@ -290,7 +290,7 @@ class EloadQC(Eload):
                 analysis_accession, f"annotation.*{analysis_accession}*.log", "annotate_variants", failed_analysis
             )
             # Statistics is only run if the aggregation is set to none
-            if self.eload_cfg.query('ingestion', 'aggregation', analysis_accession) == 'none':
+            if self.eload_cfg.query('ingestion', 'aggregation', analysis_accession, ret_default='none') == 'none':
                 self._find_log_and_check_job(
                     analysis_accession, f"statistics.*{analysis_accession}*.log", "calculate_statistics", failed_analysis
                 )
