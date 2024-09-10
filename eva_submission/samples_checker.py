1433f16a29c5a6242fb044b2ad399687951522c6
tcezard <tcezard@ebi.ac.uk>
2024-09-10 13:23:40 +0100

    Allow aggregation checks to be performed without opening the file with pysam

--- eva_submission/samples_checker.py
+++ eva_submission/samples_checker.py
@@ -21,7 +21,7 @@ def get_samples_from_vcf_manual(vcf_file):
     try:
         for line in open_file:
             if line.startswith('#CHROM'):
-                sp_line = line.split('\t')
+                sp_line = line.strip().split('\t')
                 if len(sp_line) > 9:
                     return sp_line[9:]
     finally:
