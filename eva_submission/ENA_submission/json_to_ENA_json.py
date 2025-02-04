948f187507fba1cae15db99699f269a90676015a
nitin-ebi <79518737+nitin-ebi@users.noreply.github.com>
2025-02-04 13:29:54 +0000

    EVA-3699 EVA json to ENA Json Converter (#236)
    * add script to convert eva metadata json to ena json for brokering

---
+++ eva_submission/ENA_submission/json_to_ENA_json.py
@@ -0,0 +1,277 @@
+import json
+import os.path
+import re
+from collections import defaultdict
+from datetime import datetime, timedelta
+from functools import cached_property
+
+from ebi_eva_common_pyutils.logger import AppLogger
+from ebi_eva_common_pyutils.taxonomy.taxonomy import get_scientific_name_from_ensembl
+
+from eva_submission.eload_utils import check_project_format, check_existing_project_in_ena, is_single_insdc_sequence
+
+
+def today():
+    return datetime.today()
+
+
+class EnaJsonConverter(AppLogger):
+
+    def __init__(self, submission_id, input_eva_json, output_folder, output_file_name):
+        self.submission_id = submission_id
+        self.input_eva_json = input_eva_json
+        self.output_folder = output_folder
+        self.output_file_name = output_file_name
+        with open(self.input_eva_json, 'r') as file:
+            self.eva_json_data = json.load(file)
+
+        self.output_ena_json_file = os.path.join(self.output_folder, self.output_file_name + '.json')
+
+    def create_ena_submission(self):
+        ena_json_data = {}
+
+        ena_projects_json_obj = (
+            [self._create_ena_project_json_obj(self.eva_json_data['project'], self.submission_id)]
+            if not self.is_existing_project else []
+        )
+
+        ena_analysis_json_obj = self._create_ena_analysis_json_obj()
+        ena_submission_json_obj = self._create_ena_submission_json_obj(
+            self.eva_json_data['project'], self.submission_id
+        )
+
+        ena_json_data.update({
+            'submission': ena_submission_json_obj,
+            **({'projects': ena_projects_json_obj} if ena_projects_json_obj else {}),
+            'analysis': ena_analysis_json_obj
+        })
+
+        self.write_to_json(ena_json_data, self.output_ena_json_file)
+
+        return self.output_ena_json_file
+
+    def write_to_json(self, json_data, json_file):
+        with open(json_file, 'w') as file:
+            json.dump(json_data, file, indent=4)
+
+    def _create_ena_project_json_obj(self, project_data, submission_id):
+        project_alias = (f"{self.existing_project}_{submission_id}" if self.is_existing_project else submission_id)
+
+        project_accession = self.existing_project
+        project_title = project_data.get("title", "Unknown Title")
+        project_description = project_data.get("description", "No description provided")
+        project_centre_name = project_data.get("centre", "Unknown Centre")
+
+        publication_links = [
+            {"xrefLink": {"db": pub.split(":")[0], "id": pub.split(":")[1]}}
+            for pub in project_data.get("publications", [])
+            if ":" in pub
+        ]
+
+        tax_id = project_data.get("taxId", 0)
+        scientific_name = get_scientific_name_from_ensembl(str(tax_id).strip())
+
+        related_projects = []
+        for key, field_name in [
+            ("parentProject", "parentProject"),
+            ("childProjects", "childProject"),
+            ("peerProjects", "peerProject"),
+        ]:
+            values = project_data.get(key, [])
+            if isinstance(values, str):
+                related_projects.append({field_name: values})
+            elif isinstance(values, list):
+                related_projects.extend([{field_name: value} for value in values])
+
+        project_links = [self.get_link(link) for link in project_data.get("links", [])]
+
+        ena_project_obj = {
+            "accession": project_accession,
+            "alias": project_alias,
+            "title": project_title,
+            "description": project_description,
+            "centreName": project_centre_name,
+            "publicationLinks": publication_links,
+            "sequencingProject": {},
+            "organism": {
+                "taxonId": tax_id,
+                "scientificName": scientific_name,
+            },
+            "relatedProjects": related_projects,
+            "projectLinks": project_links,
+        }
+
+        return ena_project_obj
+
+    def _create_ena_analysis_json_obj(self, ):
+        samples_per_analysis = self._samples_per_analysis(self.eva_json_data.get('sample', []))
+        files_per_analysis = self._files_per_analysis(self.eva_json_data.get('files', []))
+
+        for analysis in self.eva_json_data['analysis']:
+            samples = samples_per_analysis[analysis.get('analysisAlias')]
+            files = files_per_analysis[analysis.get('analysisAlias')]
+
+            self._add_analysis(analysis, samples, files, self.eva_json_data['project'])
+
+    def _add_analysis(self, analysis, samples, files, project):
+        def get_centre(analysis, project):
+            return analysis.get('centre') or project.get('centre')
+
+        def get_study_attr(project):
+            alias = project.get('alias', '')
+            title = project.get('title', '')
+            if re.match(r'^PRJ(EB|NA)', alias):
+                return {"accession": alias}
+            elif re.match(r'^PRJ(EB|NA)', title):
+                return {"accession": title}
+            elif alias:
+                return {"alias": alias}
+            elif title:
+                return {"alias": title}
+            return {}
+
+        def get_samples(samples):
+            return [{"accession": sample['bioSampleAccession'], "alias": sample.get('sampleInVCF')}
+                    for sample in samples]
+
+        def get_runs(analysis):
+            return analysis.get('runAccessions', [])
+
+        def get_assemblies(analysis):
+            reference_genome = analysis.get('referenceGenome', '').strip()
+            if is_single_insdc_sequence(reference_genome):
+                return [{"assembly": {"accession": reference_genome}}]
+            else:
+                assembly = {}
+                if reference_genome.split(':')[0] in ['file', 'http', 'ftp']:
+                    assembly["custom"] = {"urlLink": reference_genome}
+                else:
+                    assembly["standard"] = reference_genome
+                return [{"assembly": assembly}]
+
+        def get_experiments(analysis):
+            return [experiment.strip().capitalize()
+                    for experiment in analysis.get('experimentType', '').split(':')]
+
+        def get_analyses_attributes(analysis):
+            analysis_attributes = []
+            if analysis.get('software'):
+                analysis_attributes.append({"tag": "SOFTWARE", "value": analysis['software'].strip()})
+            if analysis.get('platform'):
+                analysis_attributes.append({"tag": "PLATFORM", "value": analysis['platform'].strip()})
+            if str(analysis.get('imputation', '')).strip() == '1':
+                analysis_attributes.append({"tag": "IMPUTATION", "value": "1"})
+
+            return analysis_attributes
+
+        def get_file_objs(files):
+            return [
+                {
+                    'fileName': file['fileName'],
+                    **({"fileType": file['fileType']} if 'fileType' in file else {}),
+                    **({"checksumMethod": 'MD5'} if 'checksum' in file else {}),
+                    **({"checksum": file['checksum']} if 'checksum' in file else {}),
+                }
+                for file in files
+            ]
+
+        def get_analysis_links(analysis):
+            return [self.get_link(link) for link in analysis.get('links', [])]
+
+        def get_attributes(analysis):
+            if analysis.get('pipelineDescriptions'):
+                return [{"tag": "pipelineDescription", "value": analysis['pipelineDescriptions'].strip()}]
+            return None
+
+        analysis_json_obj = {
+            "title": analysis['analysisTitle'],
+            "description": analysis['description'],
+            "centreName": get_centre(analysis, project),
+            "study": get_study_attr(project),
+            "samples": get_samples(samples),
+            "runs": get_runs(analysis),
+            "analysisType": "SEQUENCE_VARIATION",
+            "assemblies": get_assemblies(analysis),
+            "experiments": get_experiments(analysis),
+            "attributes": get_analyses_attributes(analysis),
+            "files": get_file_objs(files),
+            "links": get_analysis_links(analysis),
+        }
+
+        attributes = get_attributes(analysis)
+        if attributes:
+            analysis_json_obj["attributes"] = attributes
+
+        return analysis_json_obj
+
+    def _samples_per_analysis(self, samples_data):
+        samples_per_analysis = defaultdict(list)
+        for sample in samples_data:
+            for analysis_alias in sample.get('analysisAlias', []):
+                samples_per_analysis[analysis_alias.strip()].append(sample)
+        return samples_per_analysis
+
+    def _files_per_analysis(self, files_data):
+        files_per_analysis = defaultdict(list)
+        for file in files_data:
+            files_per_analysis[file.get('analysisAlias', '').strip()].append(file)
+        return files_per_analysis
+
+    def _create_ena_submission_json_obj(self, project_data, submission_id):
+        centreName = project_data.get('centre')
+
+        submission_alias = (
+            f"{self.existing_project}_{submission_id}" if self.is_existing_project else submission_id
+        )
+
+        hold_date = project_data.get('holdDate') or (today() + timedelta(days=3))
+        # Make sure the hold date is in 'YYYY-MM-DD' format
+        self.hold_date = (
+            hold_date.strftime('%Y-%m-%d') if isinstance(hold_date, datetime) else hold_date
+        )
+
+        ena_submission_obj = {
+            "alias": submission_alias,
+            "centerName": centreName,
+            "actions": [
+                {"type": "ADD"},
+                {"type": "HOLD", "holdUntilDate": self.hold_date},
+            ],
+        }
+
+        return ena_submission_obj
+
+    @cached_property
+    def is_existing_project(self):
+        return self.existing_project is not None
+
+    @cached_property
+    def existing_project(self):
+        prj_alias = self.submission_id
+        prj_title = self.eva_json_data['project']['title']
+        if check_project_format(prj_alias) and check_existing_project_in_ena(prj_alias):
+            return prj_alias
+        elif check_project_format(prj_title) and check_existing_project_in_ena(prj_title):
+            return prj_title
+        return None
+
+    def get_link(self, link):
+        if re.match(r'^(ftp:|http:|file:|https:)', link):
+            sp_link = link.split('|', maxsplit=1)
+            url = sp_link[0]
+            label = sp_link[1] if len(sp_link) > 1 else url
+            return {
+                "urlLink": {
+                    "label": label,
+                    "url": url
+                }
+            }
+        else:
+            link_parts = link.split(':', maxsplit=2)
+            return {
+                "xrefLink": {
+                    "db": link_parts[0],
+                    **({"id": link_parts[1]} if len(link_parts) > 1 else {}),
+                    **({"label": link_parts[2]} if len(link_parts) > 2 else {})
+                }
+            }
---
+++ tests/test_eva_json_to_ena_json.py
@@ -0,0 +1,199 @@
+import json
+import os
+from datetime import datetime
+from unittest import TestCase
+from unittest.mock import patch, PropertyMock
+
+from eva_submission import ROOT_DIR
+from eva_submission.ENA_submission.json_to_ENA_json import EnaJsonConverter
+
+
+class TestEVAJsonToENAJsonConverter(TestCase):
+
+    def setUp(self) -> None:
+        self.brokering_folder = os.path.join(ROOT_DIR, 'tests', 'resources', 'brokering')
+        self.metadata_file = os.path.join(self.brokering_folder, 'eva_metadata_json.json')
+        self.converter = EnaJsonConverter('Submission-12345', self.metadata_file, self.brokering_folder,
+                                          'To_Json_Converter_Test')
+
+        self.project = {
+            "title": "Example Project",
+            "description": "An example project for demonstration purposes",
+            "centre": "University of Example",
+            "taxId": 9606,
+            "parentProject": "PRJEB00001",
+            "childProjects": ["PRJEB00002", "PRJEB00003"],
+            "peerProjects": ["PRJEB00004", "PRJEB00005"],
+            "publications": ["PubMed:123456", "PubMed:789012"],
+            "links": ["http://www.abc.com|abc", "http://xyz.com", "PubMed:123456", "PubMed:789012:abcxyz"]
+        }
+
+        self.analysis = {
+            'analysisTitle': 'Genomic Relationship Matrix',
+            'analysisAlias': 'GRM',
+            'description': 'A genomic relationship matrix (GRM) was computed',
+            'Project Title': 'TechFish - Vibrio challenge',
+            'experimentType': 'Genotyping by array',
+            'referenceGenome': 'http://abc.com',
+            'software': 'software package GCTA, Burrows-Wheeler Alignment tool (BWA), HTSeq-python package ',
+            'platform': "BGISEQ-500",
+            'imputation': "1",
+            "links": ["http://www.abc.com|abc", "http://xyz.com", "PubMed:123456", "PubMed:789012:abcxyz"],
+
+        }
+
+        self.samples = [
+            {'analysisAlias': 'GRM', 'sampleInVCF': '201903VIBRIO1185679118', 'bioSampleAccession': 'SAMEA7851610'},
+            {'analysisAlias': 'GRM', 'sampleInVCF': '201903VIBRIO1185679119', 'bioSampleAccession': 'SAMEA7851611'},
+            {'analysisAlias': 'GRM', 'sampleInVCF': '201903VIBRIO1185679120', 'bioSampleAccession': 'SAMEA7851612'},
+            {'analysisAlias': 'GRM', 'sampleInVCF': '201903VIBRIO1185679121', 'bioSampleAccession': 'SAMEA7851613'},
+            {'analysisAlias': 'GRM', 'sampleInVCF': '201903VIBRIO1185679122', 'bioSampleAccession': 'SAMEA7851614'},
+        ]
+
+        self.files = [
+            {'analysisAlias': 'GRM', 'fileName': 'Vibrio.chrom.fix2.final.debug.gwassnps.vcf.gz',
+             'fileType': 'vcf', 'md5': 'c263a486e9b273d6e1e4c5f46ca5ccb8'},
+            {'analysisAlias': 'GRM', 'fileName': 'Vibrio.chrom.fix2.final.debug.gwassnps.vcf.gz.tbi',
+             'fileType': 'tabix', 'md5': '4b61e00524cc1f4c98e932b0ee27d94e'},
+        ]
+
+    @staticmethod
+    def _delete_file(file_path):
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+    def tearDown(self) -> None:
+        self._delete_file(os.path.join(self.brokering_folder, 'To_Json_Converter_Test.json'))
+
+    def test_create_ena_project_json_obj(self):
+        expected_project_json_obj = {
+            'accession': None,
+            'alias': 'Submission-12345',
+            'title': 'Example Project',
+            'description': 'An example project for demonstration purposes',
+            'centreName': 'University of Example',
+            'publicationLinks': [
+                {'xrefLink': {'db': 'PubMed', 'id': '123456'}},
+                {'xrefLink': {'db': 'PubMed', 'id': '789012'}}
+            ],
+            'sequencingProject': {},
+            'organism': {'taxonId': 9606, 'scientificName': 'Oncorhynchus mykiss'},
+            'relatedProjects': [
+                {'parentProject': 'PRJEB00001'},
+                {'childProject': 'PRJEB00002'},
+                {'childProject': 'PRJEB00003'},
+                {'peerProject': 'PRJEB00004'},
+                {'peerProject': 'PRJEB00005'}
+            ],
+            'projectLinks': [
+                {'urlLink': {'label': 'abc', 'url': 'http://www.abc.com'}},
+                {'urlLink': {'label': 'http://xyz.com', 'url': 'http://xyz.com'}},
+                {'xrefLink': {'db': 'PubMed', 'id': '123456'}},
+                {'xrefLink': {'db': 'PubMed', 'id': '789012', 'label': 'abcxyz'}}
+            ]
+        }
+
+        with patch('eva_submission.ENA_submission.json_to_ENA_json.get_scientific_name_from_ensembl') as m_sci_name:
+            m_sci_name.return_value = 'Oncorhynchus mykiss'
+            ena_project_json_obj = self.converter._create_ena_project_json_obj(self.project,
+                                                                               "Submission-12345")
+            self.assert_json_equal(expected_project_json_obj, ena_project_json_obj)
+
+    def test_add_analysis(self):
+        expected_analysis_json_obj = {
+            'title': 'Genomic Relationship Matrix',
+            'description': 'A genomic relationship matrix (GRM) was computed',
+            'centreName': 'University of Example',
+            'study': {'alias': 'Example Project'},
+            'samples': [
+                {'accession': 'SAMEA7851610', 'alias': '201903VIBRIO1185679118'},
+                {'accession': 'SAMEA7851611', 'alias': '201903VIBRIO1185679119'},
+                {'accession': 'SAMEA7851612', 'alias': '201903VIBRIO1185679120'},
+                {'accession': 'SAMEA7851613', 'alias': '201903VIBRIO1185679121'},
+                {'accession': 'SAMEA7851614', 'alias': '201903VIBRIO1185679122'}],
+            'runs': [],
+            'analysisType': 'SEQUENCE_VARIATION',
+            'assemblies': [{"assembly": {"custom": {"urlLink": "http://abc.com"}}}],
+            'experiments': ['Genotyping by array'],
+            "attributes": [
+                {'tag': 'SOFTWARE',
+                 'value': 'software package GCTA, Burrows-Wheeler Alignment tool (BWA), HTSeq-python package'},
+                {'tag': 'PLATFORM', 'value': 'BGISEQ-500'},
+                {'tag': 'IMPUTATION', 'value': '1'}
+            ],
+            'files': [
+                {'fileName': 'Vibrio.chrom.fix2.final.debug.gwassnps.vcf.gz', 'fileType': 'vcf'},
+                {'fileName': 'Vibrio.chrom.fix2.final.debug.gwassnps.vcf.gz.tbi', 'fileType': 'tabix'}],
+            'links': [
+                {'urlLink': {'label': 'abc', 'url': 'http://www.abc.com'}},
+                {'urlLink': {'label': 'http://xyz.com', 'url': 'http://xyz.com'}},
+                {'xrefLink': {'db': 'PubMed', 'id': '123456'}},
+                {'xrefLink': {'db': 'PubMed', 'id': '789012', 'label': 'abcxyz'}}
+            ]}
+
+        ena_analysis_json_obj = self.converter._add_analysis(self.analysis, self.samples, self.files, self.project)
+        self.assert_json_equal(expected_analysis_json_obj, ena_analysis_json_obj)
+
+    def test_add_analysis_to_existing_project(self):
+        self.project['alias'] = 'PRJEB00001'
+        ena_analysis_json_obj = self.converter._add_analysis(self.analysis, self.samples, self.files, self.project)
+        assert ena_analysis_json_obj['study']["accession"] == 'PRJEB00001'
+
+    def test_create_submission_json_obj(self):
+        expected_submission_json_obj = {
+            "alias": 'Submission-12345',
+            'centerName': 'University of Example',
+            "actions": [
+                {
+                    "type": "ADD"
+                },
+                {
+                    "type": "HOLD",
+                    "holdUntilDate": "2025-01-04",
+                }
+            ]
+        }
+
+        with patch('eva_submission.ENA_submission.json_to_ENA_json.today',
+                   return_value=datetime(year=2025, month=1, day=1)):
+            ena_submission_json_obj = self.converter._create_ena_submission_json_obj(self.project,
+                                                                                     'Submission-12345')
+            self.assert_json_equal(expected_submission_json_obj, ena_submission_json_obj)
+
+    def test_create_submission_json_obj_with_date(self):
+        self.project['holdDate'] = "2023-06-25"
+        expected_submission_json_obj = {
+            "alias": 'Submission-12345',
+            'centerName': 'University of Example',
+            "actions": [
+                {
+                    "type": "ADD"
+                },
+                {
+                    "type": "HOLD",
+                    "holdUntilDate": "2023-06-25",
+                }
+            ]
+        }
+
+        ena_submission_json_obj = self.converter._create_ena_submission_json_obj(self.project,
+                                                                                 'Submission-12345')
+        self.assert_json_equal(expected_submission_json_obj, ena_submission_json_obj)
+
+    def test_create_ena_json_file(self):
+        output_ena_json = self.converter.create_ena_submission()
+        assert os.path.exists(output_ena_json)
+
+    def test_create_submission_json_obj_for_existing_project(self):
+        with patch.object(EnaJsonConverter, 'existing_project', new_callable=PropertyMock(return_value='PRJEB00001')):
+            output_ena_json = self.converter.create_ena_submission()
+            assert os.path.exists(output_ena_json)
+
+            with open(output_ena_json, 'r') as file:
+                ena_json_data = json.load(file)
+                assert 'submission' in ena_json_data
+                assert ena_json_data['submission']['alias'] == 'PRJEB00001_Submission-12345'
+
+    def assert_json_equal(self, json1, json2):
+        if json.dumps(json1, sort_keys=True) != json.dumps(json2, sort_keys=True):
+            raise AssertionError(f"JSON objects are not equal.\nExpected: {json1}\nGot: {json2}")
