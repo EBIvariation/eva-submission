55bf19de11fdf3984f3fbb2e684591f85b0a03cf
nkumar2 <nkumar2@ebi.ac.uk>
2025-03-18 00:42:42 +0000

    add script for deleting submission

---
+++ eva_submission/eload_deletion.py
@@ -0,0 +1,95 @@
+import os
+import shutil
+import tarfile
+from pathlib import Path
+
+from ebi_eva_common_pyutils.config import cfg
+
+from eva_submission.eload_submission import Eload
+from eva_submission.submission_in_ftp import deposit_box
+
+
+class EloadDeletion(Eload):
+    def __init__(self, eload_number):
+        super().__init__(eload_number)
+        self.project_accession = self.eload_cfg.query('brokering', 'ena', 'PROJECT')
+        self.project_dir = os.path.join(cfg['projects_dir'], self.project_accession)
+
+    def delete_submission(self, ftp_box, submitter):
+        self.archive_eload()
+
+        # delete
+        ftp_dir = deposit_box(ftp_box, submitter)
+        self.delete_ftp_dir(ftp_dir)
+        self.delete_project_dir(self.project_dir)
+        self.delete_eload_dir(self.eload_dir)
+
+    def is_compressed(self, file_name):
+        compressed_exts = (".gz", ".xz", ".bz2", ".zip", ".rar", ".7z")
+        return file_name.endswith(compressed_exts)
+
+    def archive_eload(self):
+        archive_dir = os.path.join(self.eload_dir, 'archive_dir')
+        os.makedirs(archive_dir, exist_ok=True)
+        archive_file = os.path.join(self.eload_dir, f'{self.eload}.tar')
+        # copy relevant files to the archive_dir
+        self.copy_eload_files(archive_dir)
+
+        # archive eload
+        with tarfile.open(archive_file, "w:gz") as tar:
+            for root, _, files in os.walk(archive_dir):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    arcname = os.path.relpath(file_path, start=archive_dir)
+                    if self.is_compressed(file):
+                        tar.add(file_path, arcname=arcname, recursive=False, filter=lambda x: x)
+                    else:
+                        tar.add(file_path, arcname=arcname)
+
+        # copy to lts
+        shutil.copy(archive_file, cfg['eloads_lts_dir'])
+
+    def copy_eload_files(self, archive_dir):
+        # copy config file
+        shutil.copy(self.config_path, archive_dir)
+
+        # copy submission logs
+        for file in Path(self.eload_dir).glob("*_submission.log"):
+            shutil.copy(file, archive_dir)
+
+        # copy metadata spreadsheet and vcf files along with index
+        src_ena_dir = os.path.join(self.eload_dir, '18_brokering/ena')
+        archive_ena_dir = os.path.join(archive_dir, '18_brokering/ena')
+        os.makedirs(archive_ena_dir, exist_ok=True)
+        shutil.copy(os.path.join(src_ena_dir, 'metadata_spreadsheet.xlsx'), archive_ena_dir)
+        for file in Path(src_ena_dir).glob("*.vcf.gz"):
+            shutil.copy(file, archive_ena_dir)
+        for file in Path(src_ena_dir).glob("*.vcf.csi"):
+            shutil.copy(file, archive_ena_dir)
+
+        # copy 00_logs
+        src_log_dir = os.path.join(self.eload_dir, '00_logs')
+        archive_log_dir = os.path.join(archive_dir, '00_logs')
+        shutil.copytree(src_log_dir, archive_log_dir)
+
+        # copy accessioned files from 60_eva_public
+        src_accessioned_files_dir = os.path.join(self.eload_dir, "60_eva_public")
+        archive_accession_files_dir = os.path.join(archive_dir, "60_eva_public")
+        os.makedirs(archive_accession_files_dir, exist_ok=True)
+        for file in Path(src_accessioned_files_dir).glob("*.accessioned.vcf.gz"):
+            shutil.copy(file, archive_accession_files_dir)
+
+    def delete_ftp_dir(self, ftp_dir):
+        self.info(f'Deleting FTP directory {ftp_dir}')
+        if os.path.exists(ftp_dir):
+            shutil.rmtree(ftp_dir)
+
+    def delete_project_dir(self, project_dir):
+        self.info(f'Deleting Project directory {project_dir}')
+        if os.path.exists(project_dir):
+            shutil.rmtree(project_dir)
+
+    def delete_eload_dir(self, eload_dir):
+        self.info(f'Deleting Eload directory {eload_dir}')
+        if os.path.exists(eload_dir):
+            shutil.rmtree(eload_dir)
---
+++ tests/test_submission_deletion.py
@@ -0,0 +1,195 @@
+import os
+import shutil
+import tarfile
+from pathlib import Path
+from unittest import TestCase
+
+import yaml
+from ebi_eva_common_pyutils.config import cfg
+
+from eva_submission.eload_deletion import EloadDeletion
+from eva_submission.submission_config import load_config
+
+
+class TestEloadDeletion(TestCase):
+    test_top_dir = os.path.dirname(__file__)
+    sub_del_test_dir = os.path.join(test_top_dir, 'test_submission_deletion')
+
+    def setUp(self) -> None:
+        if os.path.exists(self.sub_del_test_dir):
+            shutil.rmtree(self.sub_del_test_dir)
+        os.makedirs(self.sub_del_test_dir, exist_ok=True)
+
+        # create and load config file
+        config_file = self.create_config_file(self.sub_del_test_dir)
+        load_config(config_file)
+
+        # create eload config
+        eload_number = 1
+        project_acc = "PRJEB11111"
+
+        # create eloads directory and config file
+        eload_config_dir = os.path.join(cfg['eloads_dir'], f"ELOAD_{eload_number}")
+        os.makedirs(eload_config_dir, exist_ok=True)
+        self.create_eload_config_file(eload_config_dir, eload_number, project_acc)
+
+        # Need to set the directory so that the relative path set in the config file works from the top directory
+        os.chdir(self.sub_del_test_dir)
+        self.eload_deletion = EloadDeletion(eload_number)
+
+    def tearDown(self):
+        if os.path.exists(self.sub_del_test_dir):
+            shutil.rmtree(self.sub_del_test_dir)
+
+    def create_config_file(self, config_file_dir):
+        config_file_path = os.path.join(config_file_dir, 'config.yml')
+        # remove file if already exists
+        if os.path.exists(config_file_path):
+            os.remove(config_file_path)
+
+        data = {"eloads_dir": os.path.join(self.sub_del_test_dir, "eloads"),
+                "projects_dir": os.path.join(self.sub_del_test_dir, "projects"),
+                "ftp_dir": os.path.join(self.sub_del_test_dir, "ftp"),
+                "eloads_lts_dir": os.path.join(self.sub_del_test_dir, "lts")}
+        with open(config_file_path, "w") as file:
+            yaml.dump(data, file, default_flow_style=False)
+
+        return config_file_path
+
+    def create_eload_config_file(self, config_file_dir, eload_number, project_accession):
+        config_file_path = os.path.join(config_file_dir, f'.ELOAD_{eload_number}_config.yml')
+        # remove file if already exists
+        if os.path.exists(config_file_path):
+            os.remove(config_file_path)
+
+        data = {"brokering": {"ena": {"PROJECT": project_accession}}}
+        with open(config_file_path, "w") as file:
+            yaml.dump(data, file, default_flow_style=False)
+
+    def test_delete_project_dir(self):
+        # create required directory and files
+        project_dir = os.path.join(self.sub_del_test_dir, 'project_dir', 'PRJEB11111')
+        os.makedirs(project_dir)
+        Path(f'{project_dir}/test1.txt').touch()
+        Path(f'{project_dir}/test2.txt').touch()
+        # call delete method
+        self.eload_deletion.delete_project_dir(project_dir)
+        # assert
+        assert not os.path.exists(project_dir)
+
+    def test_delete_eload_dir(self):
+        # create required directory and files
+        eload_dir = os.path.join(self.sub_del_test_dir, 'eload_dir', 'ELOAD_1')
+        os.makedirs(eload_dir)
+        Path(f'{eload_dir}/test1.txt').touch()
+        Path(f'{eload_dir}/test2.txt').touch()
+        # call method
+        self.eload_deletion.delete_eload_dir(eload_dir)
+        # assert
+        assert not os.path.exists(eload_dir)
+
+    def test_delete_ftp_dir(self):
+        # create required directory and files
+        ftp_dir = os.path.join(self.sub_del_test_dir, 'ftp_dir', 'eva-box-01')
+        os.makedirs(ftp_dir)
+        Path(f'{ftp_dir}/test1.txt').touch()
+        Path(f'{ftp_dir}/test2.txt').touch()
+        # call method
+        self.eload_deletion.delete_ftp_dir(ftp_dir)
+        # assert
+        assert not os.path.exists(ftp_dir)
+
+    def test_copy_eload_files(self):
+        # setup data
+        self.setup_test_eload_data(1)
+
+        # call method
+        archive_dir = os.path.join(self.sub_del_test_dir, 'archive_dir')
+        os.makedirs(archive_dir)
+        self.eload_deletion.copy_eload_files(archive_dir)
+
+        # assert
+        assert os.path.exists(os.path.join(archive_dir, '.ELOAD_1_config.yml'))
+        assert os.path.exists(os.path.join(archive_dir, 'ELOAD_1_submission.log'))
+        assert os.path.exists(os.path.join(archive_dir, '18_brokering', 'ena', 'metadata_spreadsheet.xlsx'))
+        assert os.path.exists(os.path.join(archive_dir, '18_brokering', 'ena', 'test_1.vcf.gz'))
+        assert os.path.exists(os.path.join(archive_dir, '18_brokering', 'ena', 'test_1.vcf.csi'))
+        assert os.path.exists(os.path.join(archive_dir, '00_logs', 'test_log_1.txt'))
+        assert os.path.exists(os.path.join(archive_dir, '00_logs', 'test_log_2.txt'))
+        assert os.path.exists(os.path.join(archive_dir, '60_eva_public', 'test_1.accessioned.vcf.gz'))
+
+        assert not os.path.exists(os.path.join(archive_dir, '60_eva_public', 'test_2.accessioned.vcf'))
+
+    def test_delete_submission(self):
+        # setup test data
+        self.setup_test_eload_data(1)
+        self.setup_test_ftp_boxes_data(1, 'test_user')
+
+        # call method
+        self.eload_deletion.delete_submission(1, 'test_user')
+
+        # assert ftp files are deleted
+        assert os.path.exists(os.path.join(self.sub_del_test_dir, 'ftp', 'eva-box-01', 'upload'))
+        assert not os.path.exists(os.path.join(self.sub_del_test_dir, 'ftp', 'eva-box-01', 'upload', 'test_user'))
+        # assert eload dir is deleted
+        assert not os.path.exists(os.path.join(self.eload_deletion.eload_dir))
+        # assert project dir is deleted
+        assert not os.path.exists(os.path.join(self.eload_deletion.project_dir))
+
+        # extract archived tar file
+        src_tar_file = os.path.join(cfg['eloads_lts_dir'], f"{self.eload_deletion.eload}.tar")
+        target_tar_file = os.path.join(self.sub_del_test_dir, f"{self.eload_deletion.eload}")
+        if os.path.exists(target_tar_file):
+            os.remove(target_tar_file)
+
+        with tarfile.open(src_tar_file, "r:*") as tar:
+            tar.extractall(path=target_tar_file)
+
+        # assert file copied
+        assert os.path.exists(os.path.join(self.sub_del_test_dir, 'ELOAD_1', '.ELOAD_1_config.yml'))
+        assert os.path.exists(os.path.join(self.sub_del_test_dir, 'ELOAD_1', 'ELOAD_1_submission.log'))
+        assert os.path.exists(
+            os.path.join(self.sub_del_test_dir, 'ELOAD_1', '18_brokering', 'ena', 'metadata_spreadsheet.xlsx'))
+        assert os.path.exists(os.path.join(self.sub_del_test_dir, 'ELOAD_1', '18_brokering', 'ena', 'test_1.vcf.gz'))
+        assert os.path.exists(os.path.join(self.sub_del_test_dir, 'ELOAD_1', '18_brokering', 'ena', 'test_1.vcf.csi'))
+        assert os.path.exists(os.path.join(self.sub_del_test_dir, 'ELOAD_1', '00_logs', 'test_log_1.txt'))
+        assert os.path.exists(os.path.join(self.sub_del_test_dir, 'ELOAD_1', '00_logs', 'test_log_2.txt'))
+        assert os.path.exists(
+            os.path.join(self.sub_del_test_dir, 'ELOAD_1', '60_eva_public', 'test_1.accessioned.vcf.gz'))
+        # assert file not copied
+        assert not os.path.exists(
+            os.path.join(self.sub_del_test_dir, 'ELOAD_1', '60_eva_public', 'test_2.accessioned.vcf'))
+
+    def setup_test_ftp_boxes_data(self, ftp_box, submitter):
+        ftp_box_path = os.path.join(cfg['ftp_dir'], 'eva-box-%02d' % ftp_box, 'upload', submitter)
+        os.makedirs(ftp_box_path, exist_ok=True)
+        Path(f"{ftp_box_path}/test_1.vcf").touch()
+
+    def setup_test_eload_data(self, eload_number):
+        # create eload config file
+        self.create_eload_config_file(self.eload_deletion.eload_dir, eload_number, 'PRJEB11111')
+
+        # create eload submission logs
+        Path(f"{self.eload_deletion.eload_dir}/ELOAD_{eload_number}_submission.log").touch()
+
+        # create metadata spreadsheet
+        Path(f"{self.eload_deletion.eload_dir}/18_brokering/ena/metadata_spreadsheet.xlsx").touch()
+
+        # create eload vcf files
+        Path(f"{self.eload_deletion.eload_dir}/18_brokering/ena/test_1.vcf.gz").touch()
+        Path(f"{self.eload_deletion.eload_dir}/18_brokering/ena/test_1.vcf.csi").touch()
+
+        # create 00_log dir and files
+        log_dir = os.path.join(self.eload_deletion.eload_dir, '00_logs')
+        os.makedirs(log_dir, exist_ok=True)
+        Path(f"{log_dir}/test_log_1.txt").touch()
+        Path(f"{log_dir}/test_log_2.txt").touch()
+
+        # create 60_eva_public dir and accessioned files
+        accessioned_files_dir = os.path.join(self.eload_deletion.eload_dir, '60_eva_public')
+        os.makedirs(accessioned_files_dir, exist_ok=True)
+        Path(f"{accessioned_files_dir}/test_1.accessioned.vcf.gz").touch()
+        Path(f"{accessioned_files_dir}/test_2.accessioned.vcf").touch()
+
+        # create lts directory
+        os.makedirs(cfg['eloads_lts_dir'])
