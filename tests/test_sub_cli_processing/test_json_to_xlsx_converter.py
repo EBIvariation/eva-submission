6b4c8f1940e3492abd210b338e80c1ab96ef8858
tcezard <tcezard@ebi.ac.uk>
2025-05-15 09:10:27 +0100

    Cast taxid as int Add default file type for VCF

--- tests/test_sub_cli_processing/test_json_to_xlsx_converter.py
+++ tests/test_sub_cli_processing/test_json_to_xlsx_converter.py
@@ -1,10 +1,12 @@
 import os
 from unittest import TestCase
+from unittest.mock import patch
 
 import openpyxl
 
 from eva_sub_cli_processing.sub_cli_to_eload_converter.json_to_xlsx_converter import JsonToXlsxConverter
 from eva_submission import ROOT_DIR
+from eva_submission.xlsx.xlsx_validation import EvaXlsxValidator
 
 
 class TestJsonToXlsxConverter(TestCase):
@@ -112,17 +114,20 @@ class TestJsonToXlsxConverter(TestCase):
         assert samples_details[3]['Sample ID'] == 'sample4'
         assert samples_details[3]['Scientific Name'] == 'Lemur catta'
         assert samples_details[3]['sex'] == 'Female'
-        assert samples_details[3]['Tax Id'] == '9447'
+        assert samples_details[3]['Tax Id'] == 9447
         assert samples_details[3]['tissue_type'] == 'skin'
 
     def assert_files_details(self, files_details):
         assert len(files_details) == 3
         assert files_details[0]['Analysis Alias'] == 'VD1'
         assert files_details[0]['File Name'] == 'example1.vcf.gz'
+        assert files_details[0]['File Type'] == 'vcf'
         assert files_details[1]['Analysis Alias'] == 'VD2'
         assert files_details[1]['File Name'] == 'example2.vcf'
+        assert files_details[1]['File Type'] == 'vcf'
         assert files_details[2]['Analysis Alias'] == 'VD3'
         assert files_details[2]['File Name'] == 'example3.vcf'
+        assert files_details[2]['File Type'] == 'vcf'
 
     def read_excel_to_dict(self, file_path):
         wb = openpyxl.load_workbook(file_path, data_only=True)
@@ -148,3 +153,15 @@ class TestJsonToXlsxConverter(TestCase):
             data_dict[sheet_name] = data
 
         return data_dict
+
+    def test_json_to_xlsx_converter_is_valid(self):
+        self.converter = JsonToXlsxConverter(self.test_input_json, self.output_xlsx)
+
+        self.converter.convert_json_to_xlsx()
+        with (
+            patch.object(EvaXlsxValidator, 'check_biosamples_accessions'),
+            patch('eva_submission.xlsx.xlsx_validation.check_existing_project_in_ena')
+        ):
+            validator = EvaXlsxValidator(self.output_xlsx)
+            validator.validate()
+            assert validator.error_list == ['Check Analysis Alias vs Samples: VD4,VD5 present in Samples not in Analysis Alias']
