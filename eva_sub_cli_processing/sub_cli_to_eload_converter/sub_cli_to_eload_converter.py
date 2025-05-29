abcc2c394e730c05caa8e78556f85382cb9286ef
tcezard <tcezard@ebi.ac.uk>
2025-05-29 13:25:24 +0100

    address review comments

--- eva_sub_cli_processing/sub_cli_to_eload_converter/sub_cli_to_eload_converter.py
+++ eva_sub_cli_processing/sub_cli_to_eload_converter/sub_cli_to_eload_converter.py
@@ -13,7 +13,7 @@ from eva_submission.submission_config import EloadConfig
 
 class SubCLIToEloadConverter(EloadPreparation):
 
-    def __init__(self, eload_number: int, config_object: EloadConfig = None, submission_id:str = None):
+    def __init__(self, eload_number: int, submission_id:str, config_object: EloadConfig = None):
         super().__init__(eload_number, config_object)
         self.submission_id = submission_id
 
@@ -31,7 +31,7 @@ class SubCLIToEloadConverter(EloadPreparation):
         return submission_account_id
 
     @property
-    def metadata_json_for_submission_id(self):
+    def metadata_json(self):
         metadata_json_data = self._submission_obj.get('metadataJson', {})
         if not metadata_json_data:
             raise ValueError(f"Metadata json retrieval: missing metadata_json field in the response for "
@@ -39,8 +39,8 @@ class SubCLIToEloadConverter(EloadPreparation):
         return metadata_json_data
 
     @property
-    def sub_cli_submission_dir_path(self):
-        return os.path.join(cfg['ftp_dir'], 'eva-sub-cli', 'upload', self.submission_account_id, self.submission_id)
+    def sub_cli_submission_dir_path(self) -> str:
+        return str(os.path.join(cfg['ftp_dir'], 'eva-sub-cli', 'upload', self.submission_account_id, self.submission_id))
 
     def check_status(self):
         status = self._submission_obj.get('submission', {}).get('status')
@@ -61,12 +61,12 @@ class SubCLIToEloadConverter(EloadPreparation):
         metadata_json_file_path = os.path.join(metadata_dir, "metadata_json.json")
         metadata_xlsx_file_path = os.path.join(metadata_dir, "metadata_xlsx.xlsx")
         # download metadata json
-        self._download_metadata_json_file_for_submission_id(metadata_json_file_path)
+        self._download_metadata_json_file(metadata_json_file_path)
         # Store path to metadata json in the eload config
         self.eload_cfg.set('submission', 'metadata_json', value=metadata_json_file_path)
         # convert metadata json to metadata xlsx
         JsonToXlsxConverter(metadata_json_file_path, metadata_xlsx_file_path).convert_json_to_xlsx()
 
-    def _download_metadata_json_file_for_submission_id(self, metadata_json_file_path):
+    def _download_metadata_json_file(self, metadata_json_file_path):
         with open(metadata_json_file_path, "w", encoding="utf-8") as open_file:
-            json.dump(self.metadata_json_for_submission_id, open_file, indent=4)
+            json.dump(self.metadata_json, open_file, indent=4)
