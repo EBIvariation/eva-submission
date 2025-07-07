936157396c0baf775f3e6746c4fec8834ee5a126
tcezard <tcezard@ebi.ac.uk>
2025-07-07 22:29:43 +0100

    Fix fake_bcftools.sh

--- tests/nextflow-tests/bin/fake_bcftools.sh
+++ tests/nextflow-tests/bin/fake_bcftools.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 
 echo "bcftools $*"
+command=$1
+shift
 
-if [[ $1 == "merge" ]]; then
-    filename=${*: -1}
-    touch $filename
+OTHER_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -o)
+      filename="$2"
+      shift # argument
+      shift # value
+      ;;
+    *)
+      OTHER_ARGS+=("$1") # save other arg
+      shift # argument
+      ;;
+  esac
+done
+
+echo "${OTHER_ARGS[*]}"
+
+if [[ $command == "merge" ]]; then
+    if [ -z $filename ]; then
+      filename=${OTHER_ARGS: -1}
+    fi
     printf "> Files merged:\n"
-    cat $5
-    printf "\n"
-elif [[ $1 == "sort" ]]; then
-    filename=${*: -2}
-    touch $filename
-elif [[ $1 == "norm" ]]; then
-    filename=${*: -2}
-    printf $filename
+elif [[ $command == "sort" ]]; then
+    # Only create the output if it is specified
+    if [ ! -z $filename ]; then
+      touch $filename
+    fi
+elif [[ $command == "norm" ]]; then
+    if [ -z $filename ]; then
+      filename=${OTHER_ARGS: -2}
+    fi
     touch $filename
 else
-    filename=$3
+    if [ -z $filename ]; then
+      filename=${OTHER_ARGS[1]}
+    fi
     touch ${filename}.csi
 fi
