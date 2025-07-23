33e90e42fb6584191f39ddacde7b7391610fab8e
tcezard <tcezard@ebi.ac.uk>
2025-07-23 11:58:01 +0100

    fix sample name

--- eva_submission/evapro/find_from_ena.py
+++ eva_submission/evapro/find_from_ena.py
@@ -63,7 +63,6 @@ class ApiEnaProjectFinder:
         xml_root = download_xml_from_ena(f'https://www.ebi.ac.uk/ena/browser/api/xml/{analysis_accession}')
         xml_samples = xml_root.xpath('/ANALYSIS_SET/ANALYSIS/SAMPLE_REF')
         samples = []
-        print(xml_samples)
         if xml_samples:
             for xml_sample in xml_samples:
                 ena_accession = xml_sample.get('accession')
@@ -75,7 +74,9 @@ class ApiEnaProjectFinder:
                         if external_id.get('namespace') == 'BioSample':
                             biosample_accession = external_id.text
                 if not sample_name and biosample_accession:
-                    sample_name = self.find_sample_aliases_per_accessions([biosample_accession])[0]
+                    sample_info = self.find_sample_aliases_per_accessions([biosample_accession])[0]
+                    if sample_info:
+                        biosample_accession, sample_name = sample_info
                 if sample_name and biosample_accession:
                     samples.append((biosample_accession, sample_name))
         else:
