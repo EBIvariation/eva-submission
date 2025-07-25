77a85b29e2bcc95de24f6cdaeb252678c0c832b1
tcezard <tcezard@ebi.ac.uk>
2025-07-25 10:27:49 +0100

    Calculate md5sum  if it is present.

--- bin/load_files.py
+++ bin/load_files.py
@@ -18,6 +18,7 @@ import logging
 import sys
 from argparse import ArgumentParser
 
+from ebi_eva_common_pyutils.command_utils import run_command_with_output
 from ebi_eva_common_pyutils.logger import logging_config as log_cfg
 
 from eva_submission.eload_backlog import EloadBacklog
@@ -29,6 +30,8 @@ from eva_submission.submission_config import load_config
 def main():
     argparse = ArgumentParser(description='Retrieve information about VCF files in the config from an ELOAD or and load it to EVAPRO')
     argparse.add_argument('--eload', type=int, help='The ELOAD number of the submission for which the samples should be loaded')
+    argparse.add_argument('--debug', action='store_true', default=False,
+                          help='Set the script to output logging information at debug level')
     args = argparse.parse_args()
 
     log_cfg.add_stdout_handler()
@@ -55,6 +58,14 @@ class HistoricalProjectFileLoader(EloadBacklog):
             analysis_accession = self.eload_cfg.query('brokering', 'ena', 'ANALYSIS', analysis_alias)
             assert analysis_accession in self.analysis_accessions, f'Analysis {analysis_accession} is not in EVAPRO'
             analysis_info = self.eload_cfg.query('brokering', 'analyses', analysis_alias)
+            for vcf_file in analysis_info.get('vcf_files'):
+                if 'md5' not in analysis_info.get('vcf_files'):
+                    # create the md5 of the vcf
+                    run_command_with_output(f'calculate the md5 for {vcf_file}',
+                                            f'md5sum {vcf_file} > {vcf_file}.md5')
+                    with open(f'{vcf_file}.md5') as open_file:
+                        md5_value = open_file.readline().strip().split()[0]
+                        self.eload_cfg.set('brokering', 'analyses',analysis_alias,vcf_file,'md5', value=md5_value)
             self.eva_project_loader.load_vcf_files_from_config(
                 project_accession=self.project_accession, analysis_accession=analysis_accession,
                 taxonomy_id=taxonomy_id, assembly_accession=analysis_info.get('assembly_accession'),
