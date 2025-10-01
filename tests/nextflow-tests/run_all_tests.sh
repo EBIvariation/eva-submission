e7be553450ed9a0c83c4ed3999d574c62836ddee
tcezard <tcezard@ebi.ac.uk>
2025-10-01 09:32:44 +0100

    Execute tests

--- tests/nextflow-tests/run_all_tests.sh
+++ tests/nextflow-tests/run_all_tests.sh
@@ -6,24 +6,24 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cwd=${PWD}
 cd ${SCRIPT_DIR}
 echo "run_tests_validation.sh"
-run_tests_validation.sh
+./run_tests_validation.sh
 
 echo "run_tests_prep_brokering.sh"
-run_tests_prep_brokering.sh
+./run_tests_prep_brokering.sh
 
 echo "run_tests_simple_archive.sh"
-run_tests_simple_archive.sh
+./run_tests_simple_archive.sh
 
 echo "run_tests_accession_and_variant_load.sh"
-run_tests_accession_and_variant_load.sh
+./run_tests_accession_and_variant_load.sh
 
 echo "run_tests_accession_and_variant_load_human.sh"
-run_tests_accession_and_variant_load_human.sh
+./run_tests_accession_and_variant_load_human.sh
 
 echo "run_tests_clustering.sh"
-run_tests_clustering.sh
+./run_tests_clustering.sh
 
 echo "run_tests_remapping_clustering.sh"
-run_tests_remapping_clustering.sh
+./run_tests_remapping_clustering.sh
 
 cd ${cwd}
