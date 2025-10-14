525f0a22a6270c5f7944eae97f7c166d604c29ee
April Shen <ashen@ebi.ac.uk>
2025-10-14 14:14:20 +0100

    add run_status in eva-sub-cli results

--- eva_submission/eload_validation.py
+++ eva_submission/eload_validation.py
@@ -257,7 +257,7 @@ class EloadValidation(Eload):
                 evidence_check_dict = results.get('evidence_type_check', {})
                 aggregation_check_dict = {}
                 for alias, evidence in evidence_check_dict.items():
-                    if alias not in ['pass', 'report_path']:
+                    if alias not in ['pass', 'report_path', 'run_status']:
                         aggregation_check_dict[self._unique_alias(alias)] = self._evidence_type_to_aggregation(evidence['evidence_type'])
                 self.eload_cfg.set('validation', 'aggregation_check', 'analyses', value=aggregation_check_dict)
 
